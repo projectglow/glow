da3edd91b65f456278e02635fb75fc3cf05b3087
Karen Feng <karen.feng@databricks.com>
2019-06-12 15:11:25 -0700

    Allow Java option to change test dir (#9)
    Signed-off-by: Henry Davidge <hhd@databricks.com>

--- core/src/test/scala/com/databricks/hls/sql/HLSTestData.scala
+++ core/src/test/scala/com/databricks/hls/sql/HLSTestData.scala
@@ -1,5 +1,9 @@
 package com.databricks.hls.sql
 
+import java.nio.file.Paths
+
 trait HLSTestData {
-  final lazy val testDataHome = s"test-data"
+  final lazy val testDataHome = Paths.get(
+    sys.props.getOrElse("test.dir", ""), "test-data"
+  ).toString
 }
