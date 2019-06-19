36484396976c70ccd4c42d7aeca794d03eca741c
Henry Davidge <henrydavidge@gmail.com>
2019-06-19 15:34:50 -0700

    Refactor file uploader to use service interface (#12)

--- core/src/main/scala/com/databricks/sql/BigFileDatasource.scala
+++ core/src/main/scala/com/databricks/sql/BigFileDatasource.scala
@@ -2,11 +2,11 @@ package com.databricks.sql
 
 import java.io.ByteArrayOutputStream
 import java.net.URI
+import java.util.ServiceLoader
 
-import scala.reflect.ClassTag
-
-import com.google.common.annotations.VisibleForTesting
+import scala.collection.JavaConverters._
 import org.apache.commons.io.IOUtils
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.sources._
@@ -67,93 +67,18 @@ object BigFileDatasource {
 
 case class SingleFileRelation(sqlContext: SQLContext, schema: StructType) extends BaseRelation
 
-private[databricks] sealed trait ParallelUploadResult {}
-
-private[databricks] sealed trait ParallelUploader[T <: ParallelUploadResult] extends Serializable {
-
-  def optMinPartSize: Option[Int] = None
-
-  final def uploadPart(os: ByteArrayOutputStream, part: Int): T = {
-    uploadPart(os.toByteArray, part)
-  }
-
-  def uploadPart(bytes: Array[Byte], part: Int): T
-
-  def finalizeUpload(parts: Seq[T])
-
-  final def finalizeUpload(parts: Array[ParallelUploadResult]) {
-    finalizeUpload(parts.map {
-      case parT: T =>
-        parT
-      case p =>
-        throw new IllegalStateException("Received illegal return type %s from upload".format(p))
-    }.toSeq)
-  }
+trait BigFileUploader {
+  def canUpload(conf: Configuration, path: String): Boolean
+  def upload(bytes: RDD[Array[Byte]], path: String): Unit
 }
 
-private[databricks] case class AmazonPartUploadResult(eTag: String, partId: Int)
-    extends ParallelUploadResult {}
-
-//private[databricks] object AmazonFileUploader {
-//
-//  def initiateUpload(path: URI, parts: Int): AmazonFileUploader = {
-//
-//    val s3 = new AmazonS3Client()
-//
-//    // quick and dirty parsing logic
-//    val bucket = path.getAuthority
-//    val key = path.getPath.drop(1) // begins with a /
-//
-//    val request = new InitiateMultipartUploadRequest(bucket, key)
-//    val uploadId = s3.initiateMultipartUpload(request).getUploadId
-//
-//    AmazonFileUploader(bucket, key, uploadId)
-//  }
-//}
-//
-//private[databricks] case class AmazonFileUploader(bucket: String, key: String, uploadId: String)
-//    extends ParallelUploader[AmazonPartUploadResult] {
-//
-//  override def optMinPartSize: Option[Int] = Some(5 * 1024 * 1024)
-//
-//  def uploadPart(bytes: Array[Byte], part: Int): AmazonPartUploadResult = {
-//
-//    val s3 = new AmazonS3Client()
-//    val bais = new ByteArrayInputStream(bytes)
-//    val response = s3.uploadPart(
-//      new UploadPartRequest()
-//        .withBucketName(bucket)
-//        .withKey(key)
-//        .withUploadId(uploadId)
-//        .withPartNumber(part + 1)
-//        .withInputStream(bais)
-//        .withPartSize(bytes.length)
-//    )
-//
-//    val eTag = response.getPartETag
-//
-//    AmazonPartUploadResult(eTag.getETag, part)
-//  }
-//
-//  def finalizeUpload(parts: Seq[AmazonPartUploadResult]) {
-//
-//    val eTags = parts.map(p => {
-//      new PartETag(p.partId + 1, p.eTag)
-//    })
-//
-//    val s3 = new AmazonS3Client()
-//
-//    s3.completeMultipartUpload(
-//      new CompleteMultipartUploadRequest()
-//        .withBucketName(bucket)
-//        .withKey(key)
-//        .withUploadId(uploadId)
-//        .withPartETags(eTags.toList.asJava)
-//    )
-//  }
-//}
-
 private[databricks] object SingleFileWriter extends HLSLogging {
+
+  lazy val uploaders: Seq[BigFileUploader] = ServiceLoader
+    .load(classOf[BigFileUploader])
+    .iterator()
+    .asScala
+    .toSeq
 
   /**
    * Writes a single file in parallel to a storage system.
@@ -164,105 +89,13 @@ private[databricks] object SingleFileWriter extends HLSLogging {
    * @param path The path to write the RDD to.
    */
   def write(rdd: RDD[Array[Byte]], path: String) {
-
     val uri = new URI(path)
-
-//    // TODO: support DBFS, Azure, s3 without IAM role configured
-//    if (uri.getScheme == "s3" ||
-//      uri.getScheme == "s3a") {
-//
-//      val uploader = AmazonFileUploader.initiateUpload(uri, rdd.partitions.length)
-//      writeFileParallel[AmazonPartUploadResult](rdd, uploader)
-//    } else {
-      logger.info(s"Writing file from driver")
-      writeFileFromDriver(new Path(uri), rdd)
-//    }
-  }
-
-  /**
-   * Given an RDD of byte arrays that should be written out in a parallel file upload, repartition
-   * it so that each partition except for the last is at least as large as the minimum partition
-   * size. Both the input and output RDDs should contain a single byte array per partition.
-   * @param rdd
-   * @param minPartitionSizeB
-   * @return
-   */
-  @VisibleForTesting
-  private[sql] def repartitionRddToUpload(
-      rdd: RDD[Array[Byte]],
-      minPartitionSizeB: Int): RDD[Array[Byte]] = {
-
-    val partitionByteSizes = rdd.map(_.length.toLong).collect
-    val totalBytes = partitionByteSizes.sum
-    val smallestPartition = partitionByteSizes.min
-
-    if (smallestPartition > minPartitionSizeB || partitionByteSizes.length == 1) {
-      // RDD is fine to upload
-      rdd
-    } else {
-      val desiredPartitionSize = Math.min(minPartitionSizeB * 2, totalBytes)
-      val partitionStartsIdxs = partitionByteSizes.dropRight(1).scan(0L)(_ + _)
-
-      val shuffle = rdd.mapPartitionsWithIndex {
-        case (idx, iter) =>
-          val bytes = iter.next()
-          require(!iter.hasNext, "Input RDD should have one byte array per partition")
-
-          val partStart = partitionStartsIdxs(idx)
-          val partEnd = partStart + bytes.length
-          val destPartStart = partStart / desiredPartitionSize
-          val destPartEnd = (partEnd - 1) / desiredPartitionSize
-
-          (destPartStart to destPartEnd).map { part =>
-            val startIndex = Math.max(0, part * desiredPartitionSize - partStart)
-            val endIndex = Math.min(bytes.length, (part + 1) * desiredPartitionSize - partStart)
-
-            // Emit the current partition as well as the desired partition so that the records
-            // in each partition are sorted correctly
-            ((idx, part.toInt), bytes.slice(startIndex.toInt, endIndex.toInt))
-          }.toIterator
-      }
-
-      shuffle
-        .repartitionAndSortWithinPartitions(
-          new ManualRegionPartitioner(Math.ceil(totalBytes / desiredPartitionSize.toDouble).toInt)
-        )
-        .mapPartitions(iter => Iterator(iter.map(_._2).reduce(_ ++ _)))
+    uploaders.find(_.canUpload(rdd.sparkContext.hadoopConfiguration, path)) match {
+      case Some(uploader) => uploader.upload(rdd, path)
+      case None =>
+        logger.info(s"Could not find a parallel uploader for $path, uploading from the driver")
+        writeFileFromDriver(new Path(uri), rdd)
     }
-  }
-
-  private def writeFileParallel[U <: ParallelUploadResult](
-      byteRdd: RDD[Array[Byte]],
-      uploader: ParallelUploader[U])(implicit uTag: ClassTag[U]) {
-
-    // how many parts will we be uploading
-    val parts = byteRdd.partitions.length
-    require(
-      parts < 10000,
-      ("Cannot do a multipart upload with more than 10000 parts. " +
-      "RDD has (%d) partitions and a header.").format(parts)
-    )
-
-    // write each partition out, and upload them in parallel
-    val rddToUpload = uploader.optMinPartSize match {
-      case None => byteRdd
-      case Some(minSize) => repartitionRddToUpload(byteRdd, minSize)
-    }
-
-    val partitionUploads = rddToUpload
-      .mapPartitionsWithIndex {
-        case (idx, iter) =>
-          // upload the part and return any metadata
-          Iterator((idx, uploader.uploadPart(iter.next(), idx)))
-      }
-      .collect()
-
-    // add the header metadata if it exists and sort the upload metadata
-    val sortedMetadata = partitionUploads
-      .sortBy(_._1)
-      .map(_._2)
-
-    uploader.finalizeUpload(sortedMetadata)
   }
 
   private def writeFileFromDriver(path: Path, byteRdd: RDD[Array[Byte]]): Unit = {
--- core/src/test/scala/com/databricks/sql/SingleFileWriterSuite.scala
+++ core/src/test/scala/com/databricks/sql/SingleFileWriterSuite.scala
@@ -1,53 +1,37 @@
 package com.databricks.sql
 
+import java.nio.file.Files
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.rdd.RDD
+
 import com.databricks.hls.sql.HLSBaseTest
 
 class SingleFileWriterSuite extends HLSBaseTest {
+  test("uses service loader") {
+    val outDir = Files.createTempDirectory("writer")
+    assert(DummyFileUploader.counter == 0)
+    SingleFileWriter.write(
+      sparkContext.emptyRDD[Array[Byte]],
+      outDir.resolve("monkey").toString)
+    assert(DummyFileUploader.counter == 1)
+    SingleFileWriter.write(
+      sparkContext.emptyRDD[Array[Byte]],
+      outDir.resolve("orangutan").toString)
+    assert(DummyFileUploader.counter == 1)
+  }
+}
 
-  def testRepartition(
-      testName: String,
-      inputByteArrays: Seq[Array[Byte]],
-      expectedByteArrays: Array[Array[Byte]],
-      minPartitionSizeB: Int): Unit = {
-
-    test(testName) {
-      val rdd = spark.sparkContext.parallelize(inputByteArrays, inputByteArrays.size)
-      val repartitioned = SingleFileWriter.repartitionRddToUpload(rdd, minPartitionSizeB)
-      assert(repartitioned.collect().deep == expectedByteArrays.deep)
-    }
+class DummyFileUploader extends BigFileUploader {
+  override def canUpload(conf: Configuration, path: String): Boolean = {
+    path.contains("monkey")
   }
 
-  // Convenience functions to avoid typing toByte a million times
-  def ba(bytes: Byte*): Array[Byte] = Array[Byte](bytes: _*)
-  def baf(len: Int, byte: Byte = 0): Array[Byte] = Array.fill(len)(byte)
+  override def upload(bytes: RDD[Array[Byte]], path: String): Unit = {
+    DummyFileUploader.counter += 1
+  }
+}
 
-  testRepartition(
-    "input byte array is uploadable",
-    Array(baf(5, 0), baf(5, 1)),
-    Array(baf(5, 0), baf(5, 1)),
-    4
-  )
-
-  testRepartition("repartition down to single", Array(ba(0), ba(1), ba(3)), Array(ba(0, 1, 3)), 10)
-
-  testRepartition(
-    "some partitions too small",
-    Array(ba(0), ba(1, 2, 3), ba(4, 5, 6)),
-    Array(ba(0, 1, 2, 3), ba(4, 5, 6)),
-    2
-  )
-
-  testRepartition(
-    "input partition same size as desired partition",
-    Array(ba(0, 1, 2, 4), ba(5)),
-    Array(ba(0, 1, 2, 4), ba(5)),
-    2
-  )
-
-  testRepartition(
-    "complex example",
-    Array(ba(0, 0, 0, 0, 1, 1), ba(1, 1), ba(2, 2, 2), ba(2, 3, 3, 3, 3), ba(4, 4, 4, 4), ba(5)),
-    (0 to 4).map(v => baf(4, v.toByte)).toArray ++ Array(ba(5)),
-    2
-  )
+object DummyFileUploader {
+  var counter = 0
 }
