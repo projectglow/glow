108277ec5c8dc86956f5a8294037bd7e57d56337
Henry Davidge <henrydavidge@gmail.com>
2019-09-09 15:07:51 -0700

    Add expression to explode a matrix by row (#52)
    * Add expression to explode a matrix by row
    
    * add more tests
    
    Signed-off-by: Henry Davidge <hhd@databricks.com>

--- core/src/main/scala/com/databricks/hls/tertiary/privateExpressionUtils.scala
+++ core/src/main/scala/com/databricks/hls/tertiary/privateExpressionUtils.scala
@@ -1,11 +1,14 @@
 package org.apache.spark.sql.databricks.hls.tertiary // Visibility hack
 
+import org.apache.spark.ml.linalg.MatrixUDT
 import org.apache.spark.sql.AnalysisException
-import org.apache.spark.sql.catalyst.analysis
-import org.apache.spark.sql.catalyst.analysis.{Star, TypeCheckResult}
-import org.apache.spark.sql.catalyst.expressions.{Alias, Expression, GetStructField, NamedExpression, Unevaluable}
+import org.apache.spark.sql.catalyst.analysis.Star
+import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
+import org.apache.spark.sql.catalyst.expressions.{Alias, ExpectsInputTypes, Expression, Generator, GenericInternalRow, GetStructField, NamedExpression, Unevaluable}
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.types.{DataType, StructType}
+import org.apache.spark.sql.catalyst.util.GenericArrayData
+import org.apache.spark.sql.catalyst.{analysis, InternalRow}
+import org.apache.spark.sql.types._
 
 /**
  * Expands all the fields of a potentially unnamed struct.
@@ -27,7 +30,7 @@ case class ExpandStruct(struct: Expression) extends Star with Unevaluable {
  * Expression that adds fields to an existing struct.
  *
  * At optimization time, this expression is rewritten as the creation of new struct with all the
- * fields of the existing struct as well as the new fields. See [[HLSReplaceExpressionsSuite]]
+ * fields of the existing struct as well as the new fields. See [[HLSReplaceExpressionsRule]]
  * for more details.
  */
 case class AddStructFields(struct: Expression, newFields: Seq[Expression])
@@ -44,5 +47,50 @@ case class AddStructFields(struct: Expression, newFields: Seq[Expression])
         base = base.add(nameStr, value.dataType, value.nullable)
     }
     base
+  }
+}
+
+/**
+ * Explodes a matrix by row. Each row of the input matrix will be output as an array of doubles.
+ *
+ * If the input expression is null or has 0 rows, the output will be empty.
+ * @param matrixExpr The matrix to explode. May be dense or sparse.
+ */
+case class ExplodeMatrix(matrixExpr: Expression)
+    extends Generator
+    with CodegenFallback
+    with ExpectsInputTypes {
+
+  val matrixUdt = new MatrixUDT()
+
+  override def children: Seq[Expression] = Seq(matrixExpr)
+
+  override def elementSchema: StructType = {
+    new StructType()
+      .add("row", ArrayType(DoubleType, containsNull = false), nullable = false)
+  }
+
+  override def inputTypes: Seq[AbstractDataType] = Seq(matrixUdt)
+
+  override def eval(input: InternalRow): TraversableOnce[InternalRow] = {
+    val matrixStruct = matrixExpr.eval(input)
+    if (matrixStruct == null) {
+      return Iterator.empty
+    }
+    val matrix = matrixUdt.deserialize(matrixStruct).toDenseRowMajor
+    var rowIdx = 0
+    new Iterator[InternalRow] {
+      override def hasNext: Boolean = rowIdx < matrix.numRows
+      override def next(): InternalRow = {
+        var colIdx = 0
+        val arr = new Array[Any](matrix.numCols)
+        while (colIdx < matrix.numCols) {
+          arr(colIdx) = matrix.values(rowIdx * matrix.numCols + colIdx)
+          colIdx += 1
+        }
+        rowIdx += 1
+        new GenericInternalRow(Array[Any](new GenericArrayData(arr)))
+      }
+    }
   }
 }
