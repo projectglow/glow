8c8a1818bdb4b59ffaca95180bd1a047f40f75b6
Henry D <henrydavidge@gmail.com>
2019-10-08 13:38:17 -0700

    sg -> glow

--- python/glow/tests/test_transform.py
+++ python/glow/tests/test_transform.py
@@ -1,12 +1,12 @@
 import pytest
 from pyspark.sql.utils import IllegalArgumentException
-import glow as sg
+import glow as glow
 
 
 def test_transform(spark):
     df = spark.read.format("vcf")\
         .load("test-data/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.vcf")
-    converted = sg.transform("pipe", df, input_formatter="vcf", output_formatter="vcf",
+    converted = glow.transform("pipe", df, input_formatter="vcf", output_formatter="vcf",
                              cmd='["cat"]', in_vcf_header="infer")
     assert converted.count() == 1075
 
@@ -15,7 +15,7 @@ def test_no_transform(spark):
     df = spark.read.format("vcf") \
         .load("test-data/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.vcf")
     with pytest.raises(IllegalArgumentException):
-        sg.transform("dne", df)
+        glow.transform("dne", df)
 
 
 def test_arg_map(spark):
@@ -27,5 +27,5 @@ def test_arg_map(spark):
         "cmd": '["cat"]',
         "in_vcfHeader": "infer"
     }
-    converted = sg.transform("pipe", df, args)
+    converted = glow.transform("pipe", df, args)
     assert converted.count() == 1075
