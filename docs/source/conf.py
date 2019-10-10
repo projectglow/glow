0bb098ae78181a14d1879c8f4485553033b6261b
Karen Feng <karen.feng@databricks.com>
2019-10-10 12:05:52 +0200

    WIP

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,9 @@ for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../../python'))
+sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('../extensions'))
+sys.path.append(os.path.abspath('../../python'))
 
 
 # -- Project information -----------------------------------------------------
