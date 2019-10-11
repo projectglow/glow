076b7303258e014c4332cc32b643e6390e5961d0
kianfar77 <kiavash.kianfar@databricks.com>
2019-10-11 16:39:06 -0700

    more docs
    Signed-off-by: kianfar77 <kiavash.kianfar@databricks.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,7 +106,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'page_width': '90%'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
