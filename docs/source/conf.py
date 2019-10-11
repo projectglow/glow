68bf259a685982a12b337706bc6cf145fedb1486
kianfar77 <kiavash.kianfar@databricks.com>
2019-10-11 11:51:44 -0700

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,16 +100,13 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    # Toc options
-    'sticky_navigation': True
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
