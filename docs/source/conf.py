bbdaec8dfc0475142d9f2829d84c290b4e4c8bce
kianfar77 <kiavash.kianfar@databricks.com>
2019-10-22 17:25:06 -0700

    revert

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'notebook'
+    'notebook',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
