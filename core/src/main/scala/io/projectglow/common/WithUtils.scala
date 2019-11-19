44f4eb789b50127393df5d76356b4c6703cc3460
Karen Feng <karen.feng@databricks.com>
2019-11-19 14:57:34 -0800

    Try on disk
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

--- core/src/main/scala/io/projectglow/common/WithUtils.scala
+++ core/src/main/scala/io/projectglow/common/WithUtils.scala
@@ -72,7 +72,7 @@ object WithUtils {
   }
 
   def withCachedRDD[T, U](rdd: RDD[T])(f: RDD[T] => U): U = {
-    rdd.persist(StorageLevel.MEMORY_AND_DISK)
+    rdd.persist(StorageLevel.DISK_ONLY)
     try {
       f(rdd)
     } finally {
@@ -81,7 +81,7 @@ object WithUtils {
   }
 
   def withCachedDataset[T, U](ds: Dataset[T])(f: Dataset[T] => U): U = {
-    ds.persist(StorageLevel.MEMORY_AND_DISK)
+    ds.persist(StorageLevel.DISK_ONLY)
     try {
       f(ds)
     } finally {
