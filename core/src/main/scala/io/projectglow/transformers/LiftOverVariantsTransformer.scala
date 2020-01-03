daad063b04c5b4248227cbce827d75b528484aa6
Karen Feng <karen.feng@databricks.com>
2020-01-02 16:22:43 -0800

    Reverse arrays during liftover if ref/alt alleles swapped (#118)
    * MVP
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Address cmts
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * AnyRef instead of Object
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Treat booleans normally
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Test with VCF with flags
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Revert flag fields
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

--- core/src/main/scala/io/projectglow/transformers/LiftOverVariantsTransformer.scala
+++ core/src/main/scala/io/projectglow/transformers/LiftOverVariantsTransformer.scala
@@ -17,6 +17,7 @@
 package io.projectglow.transformers
 
 import java.io.File
+import java.util.{ArrayList => JArrayList, Arrays => JArrays, Collections, HashMap => JHashMap}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
@@ -25,21 +26,22 @@ import htsjdk.samtools.ValidationStringency
 import htsjdk.samtools.liftover.LiftOver
 import htsjdk.samtools.reference.{ReferenceSequence, ReferenceSequenceFileFactory}
 import htsjdk.samtools.util.Interval
-import htsjdk.variant.variantcontext.VariantContext
+import htsjdk.variant.variantcontext.{Allele, Genotype, GenotypeBuilder, GenotypesContext, VariantContext, VariantContextBuilder}
 import htsjdk.variant.vcf._
+import org.apache.commons.lang.ArrayUtils
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.codegen.GenerateMutableProjection
 import org.apache.spark.sql.catalyst.expressions.{BoundReference, Literal, MutableProjection}
-import org.apache.spark.sql.types.{BooleanType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{ArrayType, BooleanType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, SQLUtils}
 import org.apache.spark.unsafe.types.UTF8String
 import picard.util.LiftoverUtils
 import picard.vcf.LiftoverVcf
 
 import io.projectglow.DataFrameTransformer
-import io.projectglow.common.{GlowLogging, VariantSchemas}
+import io.projectglow.common.{GenotypeFields, GlowLogging, VariantSchemas}
 import io.projectglow.sql.expressions.LiftOverCoordinatesExpr
-import io.projectglow.vcf.{InternalRowToVariantContextConverter, VCFSchemaInferrer, VCFWriterUtils, VariantContextToInternalRowConverter}
+import io.projectglow.vcf.{InternalRowToVariantContextConverter, VCFSchemaInferrer, VariantContextToInternalRowConverter}
 
 /**
  * Performs lift over from a variant on the reference sequence to a query sequence. Similar to
@@ -69,7 +71,8 @@ class LiftOverVariantsTransformer extends DataFrameTransformer {
         VariantSchemas.endField,
         VariantSchemas.refAlleleField))
     val inputHeaderLines = VCFSchemaInferrer.headerLinesFromSchema(inputSchema).toSet
-
+    val attributesToSwap = getAttributesToSwap(df.schema)
+    val extendedAttributesToSwap = getExtendedAttributesToSwap(df.schema)
     val outputHeaderLines = inputHeaderLines ++ picardInfoHeaderLines
     val outputSchema = getOutputSchema(inputSchema)
     val liftOverStatusColIdx = outputSchema.length - 1
@@ -100,7 +103,13 @@ class LiftOverVariantsTransformer extends DataFrameTransformer {
           // If conversion fails, use the input row and set the added liftover INFO columns to null
           (inputRowInOutputSchema, false, Some(conversionErrorMessage))
         } else {
-          liftVariantContext(inputVcOpt.get, liftOver, minMatchRatio, refSeqMap) match {
+          liftVariantContext(
+            inputVcOpt.get,
+            liftOver,
+            minMatchRatio,
+            refSeqMap,
+            attributesToSwap,
+            extendedAttributesToSwap) match {
             case (Some(outputVc), None) =>
               // If liftover succeeds, convert the output row to the output schema using the input row as a prior
               val liftedRow =
@@ -206,7 +215,9 @@ object LiftOverVariantsTransformer extends GlowLogging {
       inputVc: VariantContext,
       liftOver: LiftOver,
       minMatchRatio: Double,
-      refSeqMap: Map[String, ReferenceSequence]): (Option[VariantContext], Option[String]) = {
+      refSeqMap: Map[String, ReferenceSequence],
+      infoFieldsToSwap: Seq[String],
+      formatFieldsToSwap: Seq[String]): (Option[VariantContext], Option[String]) = {
 
     val inputInterval = new Interval(
       inputVc.getContig,
@@ -248,12 +259,9 @@ object LiftOverVariantsTransformer extends GlowLogging {
     if (outputVc.getReference.getBaseString.toLowerCase != refStr.toLowerCase) {
       if (outputVc.isBiallelic && outputVc.isSNP && refStr.equalsIgnoreCase(
           outputVc.getAlternateAllele(0).getBaseString)) {
-        val swappedRefAlt =
-          LiftoverUtils.swapRefAlt(
-            VCFWriterUtils.convertVcAttributesToStrings(outputVc).make,
-            LiftoverUtils.DEFAULT_TAGS_TO_REVERSE,
-            LiftoverUtils.DEFAULT_TAGS_TO_DROP)
-        return (Some(swappedRefAlt), None)
+        val swappedArrayFields =
+          swapRefAlt(outputVc, infoFieldsToSwap, formatFieldsToSwap)
+        return (Some(swappedArrayFields), None)
       }
       val attemptedLocus = s"${outputVc.getContig}:${outputVc.getStart}-${outputVc.getEnd}"
       return (
@@ -263,6 +271,87 @@ object LiftOverVariantsTransformer extends GlowLogging {
     }
 
     (Some(outputVc), None)
+  }
+
+  // Includes the same functionality as [[LiftoverUtils.swapRefAlt]]; also reverses array-based INFO/FORMAT fields
+  def swapRefAlt(
+      vc: VariantContext,
+      infoFieldsToSwap: Seq[String],
+      formatFieldsToSwap: Seq[String]): VariantContext = {
+    val vcb = new VariantContextBuilder(vc)
+    vcb.attribute(LiftoverUtils.SWAPPED_ALLELES, true)
+    vcb.alleles(vc.getAlleles.get(1).getBaseString, vc.getAlleles.get(0).getBaseString)
+
+    val alleleMap = new JHashMap[Allele, Allele]()
+
+    // A mapping from the old allele to the new allele, to be used when fixing the genotypes
+    alleleMap.put(vc.getAlleles.get(0), vcb.getAlleles.get(1))
+    alleleMap.put(vc.getAlleles.get(1), vcb.getAlleles.get(0))
+
+    // Drop INFO field MAX_AF
+    LiftoverUtils.DEFAULT_TAGS_TO_DROP.asScala.foreach { k =>
+      vcb.rmAttribute(k)
+    }
+
+    // Invert INFO field AF
+    LiftoverUtils.DEFAULT_TAGS_TO_REVERSE.asScala.foreach { k =>
+      if (vc.hasAttribute(k)) {
+        vcb.attribute(k, vc.getAttributeAsDoubleList(k, 0).asScala.map(1 - _).toList.asJava)
+      }
+    }
+
+    // Reverse other array-based INFO fields
+    infoFieldsToSwap.foreach { k =>
+      val arrList = vc.getAttributeAsList(k)
+      Collections.reverse(arrList)
+      vcb.attribute(k, arrList)
+    }
+
+    val swappedGenotypes = GenotypesContext.create(vc.getGenotypes().size())
+    var i = 0
+    while (i < vc.getGenotypes.size) {
+      val genotype = vc.getGenotypes.get(i)
+
+      val swappedAlleles = new JArrayList[Allele]()
+      var j = 0
+      while (j < genotype.getAlleles.size) {
+        val allele = genotype.getAllele(j)
+        if (allele.isNoCall) {
+          swappedAlleles.add(allele)
+        } else {
+          swappedAlleles.add(alleleMap.get(allele))
+        }
+        j += 1
+      }
+      val gb = new GenotypeBuilder(genotype).alleles(swappedAlleles)
+
+      // Special case: AD is not included in the extended attributes
+      if (genotype.hasAD) {
+        val ad = genotype.getAD
+        ArrayUtils.reverse(ad)
+        gb.AD(ad)
+      }
+
+      // Special case: PL is not included in the extended attributes
+      if (genotype.hasPL) {
+        val pl = genotype.getPL
+        ArrayUtils.reverse(pl)
+        gb.PL(pl)
+      }
+
+      // Reverse array-based FORMAT fields
+      formatFieldsToSwap.foreach { k =>
+        if (genotype.hasExtendedAttribute(k)) {
+          val arrList =
+            JArrays.asList(genotype.getExtendedAttribute(k).asInstanceOf[Array[AnyRef]]: _*)
+          Collections.reverse(arrList)
+          gb.attribute(k, arrList)
+        }
+      }
+      swappedGenotypes.add(gb.make())
+      i += 1
+    }
+    vcb.genotypes(swappedGenotypes).make
   }
 
   // The output schema consists of the input schema + INFO fields added by Picard + a liftOver status column
@@ -279,5 +368,55 @@ object LiftOverVariantsTransformer extends GlowLogging {
 
     GenerateMutableProjection.generate(
       passThroughExpressions ++ picardStructFieldLiterals :+ statusStructFieldLiteral)
+  }
+
+  def getArrayFields(fields: Seq[StructField]): Seq[String] = {
+    fields.flatMap { f =>
+      if (f.dataType.isInstanceOf[ArrayType]) {
+        Some(f.name)
+      } else {
+        None
+      }
+    }
+  }
+
+  // Get array-based INFO fields
+  def getAttributesToSwap(variantSchema: StructType): Seq[String] = {
+    val infoFields = variantSchema.flatMap { f =>
+      if (f.name.startsWith(VariantSchemas.infoFieldPrefix)) {
+        val name = f.name.substring(VariantSchemas.infoFieldPrefix.length)
+        if (LiftoverUtils
+            .DEFAULT_TAGS_TO_DROP
+            .contains(name) || LiftoverUtils.DEFAULT_TAGS_TO_REVERSE.contains(name)) {
+          None
+        } else {
+          Some(f.copy(name = f.name.substring(VariantSchemas.infoFieldPrefix.length)))
+        }
+      } else {
+        None
+      }
+    }
+    getArrayFields(infoFields)
+  }
+
+  // Get array-based FORMAT fields
+  def getExtendedAttributesToSwap(variantSchema: StructType): Seq[String] = {
+    val genotypeSchemaOpt = InternalRowToVariantContextConverter.getGenotypeSchema(variantSchema)
+
+    if (genotypeSchemaOpt.isEmpty) {
+      return Seq.empty
+    }
+
+    val formatFields = genotypeSchemaOpt.get.flatMap { f =>
+      val name = GenotypeFields.reverseAliases.getOrElse(f.name, f.name)
+      // Special cases: FT, GT, GQ, DP, AD and PL are not included in the extended attributes
+      if (Genotype.PRIMARY_KEYS.contains(name)) {
+        None
+      } else {
+        Some(f.copy(name = name))
+      }
+    }
+
+    getArrayFields(formatFields)
   }
 }
