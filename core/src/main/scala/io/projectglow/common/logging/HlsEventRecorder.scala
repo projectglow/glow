1dcd21ccc86ac56f958d13ac206a61fec7416926
Karen Feng <karen.feng@databricks.com>
2020-01-08 10:35:22 -0800

    Record one big VCF write event per file (#128)
    * WIP
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Whoops
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Small simplification
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Make serializable
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Cleanup
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Special case for empty map
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Cleanup
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Address comments
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

---
+++ core/src/main/scala/io/projectglow/common/logging/HlsEventRecorder.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 The Glow Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.projectglow.common.logging
+
+trait HlsEventRecorder extends HlsUsageLogging {
+
+  // Wrapper to simplify recording an HLS usage event
+  def recordHlsEvent(tag: String, options: Map[String, Any] = Map.empty): Unit = {
+    val metric = HlsMetricDefinitions.EVENT_HLS_USAGE
+    val tags = Map(HlsTagDefinitions.TAG_EVENT_TYPE -> tag)
+
+    if (options.isEmpty) {
+      recordHlsUsage(metric, tags)
+    } else {
+      recordHlsUsage(metric, tags, blob = hlsJsonBuilder(options))
+    }
+  }
+
+}
