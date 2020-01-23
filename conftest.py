9c535c0d296f9f3eeb108b6ae2c3785ea108e96c
Karen Feng <karen.feng@databricks.com>
2020-01-23 10:34:11 -0800

    Fix conftest to match removal of ADAM
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

--- conftest.py
+++ conftest.py
@@ -7,11 +7,6 @@ import pytest
 def spark():
     sess = SparkSession.builder \
         .master("local[2]") \
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
-        .config("spark.driver.maxResultSize", "0") \
-        .config("spark.kryo.registrator", "org.broadinstitute.hellbender.engine.spark.GATKRegistrator") \
-        .config("spark.kryoserializer.buffer.max", "2047m") \
-        .config("spark.kryo.registrationRequired", "false") \
         .config(
             "spark.hadoop.io.compression.codecs",
             "org.seqdoop.hadoop_bam.util.BGZFCodec,org.seqdoop.hadoop_bam.util.BGZFEnhancedGzipCodec") \
