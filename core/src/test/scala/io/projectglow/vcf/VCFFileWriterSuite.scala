03b21141535db5ee3f24b807be1c26422420e401
Karen Feng <karen.feng@databricks.com>
2020-01-27 17:49:41 -0800

    Fix CSQ parsing for synonymous variants (#145)
    * allow missing second val for string struct
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Add test
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * reformat vcfs
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    Signed-off-by: Henry Davidge <hhd@databricks.com>

--- core/src/test/scala/io/projectglow/vcf/VCFFileWriterSuite.scala
+++ core/src/test/scala/io/projectglow/vcf/VCFFileWriterSuite.scala
@@ -125,6 +125,15 @@ abstract class VCFFileWriterSuite(val sourceName: String)
 
   gridTest("Read VEP VCF with VCF parser")(schemaOptions) { schema =>
     val (ds, rewrittenDs) =
+      writeAndRereadWithDBParser(s"$testDataHome/vcf/vep.vcf", schemaOption = schema)
+    ds.collect.zip(rewrittenDs.collect).foreach {
+      case (vc1, vc2) =>
+        assert(vc1.equals(vc2), s"VC1 $vc1 VC2 $vc2")
+    }
+  }
+
+  gridTest("Read Loftee VCF with VCF parser")(schemaOptions) { schema =>
+    val (ds, rewrittenDs) =
       writeAndRereadWithDBParser(s"$testDataHome/vcf/loftee.vcf", schemaOption = schema)
     ds.collect.zip(rewrittenDs.collect).foreach {
       case (vc1, vc2) =>
