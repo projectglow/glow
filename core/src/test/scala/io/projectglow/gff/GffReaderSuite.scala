469488d0cd8868a6557d3115636aa5fed6832d98
kianfar77 <kiavash.kianfar@databricks.com>
2020-04-07 10:15:09 -0700

    more tests
    Signed-off-by: kianfar77 <kiavash.kianfar@databricks.com>

--- core/src/test/scala/io/projectglow/gff/GffReaderSuite.scala
+++ core/src/test/scala/io/projectglow/gff/GffReaderSuite.scala
@@ -21,11 +21,15 @@ import io.projectglow.gff.GffFileFormat._
 import io.projectglow.sql.GlowBaseTest
 
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions.expr
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 class GffReaderSuite extends GlowBaseTest {
   lazy val testRoot = s"$testDataHome/gff"
-  lazy val testGff3 = s"$testRoot/testgffAttWithFasta.gff"
+  lazy val testGff3 = s"$testRoot/test_gff_with_fasta.gff"
+  lazy val testGff3Gzip = s"$testRoot/test_gff_with_fasta_gzip.gff.gz"
+  lazy val testGff3Bgzip = s"$testRoot/test_gff_with_fasta_bgzip.gff.bgz"
+  lazy val testGffEmpty = s"$testRoot/test_gff_empty.gff"
 
   private val sourceName = "gff"
 
@@ -59,11 +63,13 @@ class GffReaderSuite extends GlowBaseTest {
     assert(df.schema.equals(expectedSchema))
   }
 
-  test("Read gff3") {
+  gridTest("Read gff3")(
+    Seq(testGff3, testGff3Gzip, testGff3Bgzip)
+  ) { file =>
     val dfRow = spark
       .read
       .format(sourceName)
-      .load(s"$testRoot/testgffAttWithFasta.gff")
+      .load(file)
       .orderBy("seqId", "start")
       .take(1)(0)
 
@@ -97,6 +103,7 @@ class GffReaderSuite extends GlowBaseTest {
     assert(dfRow == expectedRow)
   }
 
+
   test("updateAttFieldsWithParsedTags") {
     val currentToken = ParsedAttributesToken(None, Set(idField.name, nameField.name))
     val attributes = s"${idField.name}=1234;${aliasField.name}=monkey"
@@ -117,13 +124,4 @@ class GffReaderSuite extends GlowBaseTest {
     assert(e.getMessage.contains("GFF data source does not currently support writing."))
   }
 
-  test("gff") {
-    val df = spark.read
-    // .schema(StructType(gffBaseSchema.fields :+ attributesField))
-      .format(sourceName)
-      .load(s"$testRoot/testgffAttWithFasta.gff")
-    // .load(s"$testRoot/GCF_000001405.39_GRCh38.p13_genomic.gff.bgz")
-
-    df.show()
-  }
 }
