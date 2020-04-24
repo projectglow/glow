fa33344861bc477c8adc03be59599099467675ae
herman <herman@databricks.com>
2020-04-25 00:22:20 +0200

    Use different ExpressionInfo constructor
    Signed-off-by: herman <herman@databricks.com>

--- core/src/main/shim/3.0/SparkShim.scala
+++ core/src/main/shim/3.0/SparkShim.scala
@@ -42,16 +42,13 @@ object SparkShim extends SparkShimBase {
       examples: String,
       note: String,
       since: String): ExpressionInfo = {
+    // TODO fix this up later.
     new ExpressionInfo(
       className,
       db,
       name,
       usage,
-      arguments,
-      examples,
-      note,
-      since,
-      "" // deprecated
+      arguments
     )
   }
 
