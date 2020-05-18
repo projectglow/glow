514889a43adc48e7de1ba071e338e05dcfe88d05
Karen Feng <karen.feng@databricks.com>
2020-05-18 12:00:14 -0700

    Support flat array
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

--- python/glow/conversions.py
+++ python/glow/conversions.py
@@ -1,10 +1,63 @@
 import numpy as np
+from py4j.java_collections import JavaArray
 from py4j.protocol import register_input_converter
 from pyspark import SparkContext
+from typeguard import check_argument_types, check_return_type
 
-class NdarrayConverter(object):
+
+def _is_numpy_double_array(object, dimensions: int) -> bool:
+    assert check_argument_types()
+    output = isinstance(object, np.ndarray) and len(object.shape) == dimensions and object.dtype.type == np.double
+    assert check_return_type(output)
+    return output
+
+
+def _convert_numpy_to_java_array(np_arr: np.ndarray) -> JavaArray:
     """
-    Replaces any 2-dimensional numpy array of doubles with a DenseMatrix.
+    Converts a flat numpy array of doubles to a Java array of doubles.
+    """
+    assert check_argument_types()
+    assert len(np_arr.shape) == 1
+    assert np_arr.dtype.type == np.double
+
+    sc = SparkContext._active_spark_context
+    java_arr = sc._gateway.new_array(sc._jvm.double, np_arr.shape[0])
+    for idx, ele in enumerate(np_arr):
+        java_arr[idx] = ele.item()
+
+    assert check_return_type(java_arr)
+    return java_arr
+
+
+class OneDimensionalDoubleNumpyArrayConverter(object):
+    """
+    Replaces any 1-dimensional numpy array of doubles with a literal Java array.
+
+    Added in version 0.4.0.
+
+    Examples:
+        >>> import numpy as np
+        >>> from pyspark.sql.functions import lit
+        >>> from pyspark.sql.types import StringType
+        >>> str_list = ['a', 'b']
+        >>> df = spark.createDataFrame(str_list, StringType())
+        >>> ndarray = np.array([1.0, 2.1, 3.2])
+        >>> df.withColumn("array", lit(ndarray)).collect()
+        [Row(value='a', array=[1.0, 2.1, 3.2]), Row(value='b', array=[1.0, 2.1, 3.2])]
+    """
+
+    def can_convert(self, object):
+        return _is_numpy_double_array(object, dimensions = 1)
+
+    def convert(self, object, gateway_client):
+        sc = SparkContext._active_spark_context
+        java_arr = _convert_numpy_to_java_array(object)
+        return java_arr
+
+
+class TwoDimensionalDoubleNumpyArrayConverter(object):
+    """
+    Replaces any 2-dimensional numpy array of doubles with a literal DenseMatrix.
 
     Added in version 0.4.0.
 
@@ -20,14 +73,12 @@ class NdarrayConverter(object):
     """
 
     def can_convert(self, object):
-        return isinstance(object, np.ndarray) and len(object.shape) == 2 and object.dtype.type == np.double
+        return _is_numpy_double_array(object, dimensions = 2)
 
     def convert(self, object, gateway_client):
         sc = SparkContext._active_spark_context
         flat_arr = object.ravel()
-        java_arr = sc._gateway.new_array(sc._jvm.double, flat_arr.shape[0])
-        for idx, ele in enumerate(flat_arr):
-            java_arr[idx] = ele.item()
+        java_arr = _convert_numpy_to_java_array(flat_arr)
         dense_matrix = sc._jvm.org.apache.spark.ml.linalg.DenseMatrix(object.shape[0], object.shape[1], java_arr)
         matrix_udt = sc._jvm.org.apache.spark.ml.linalg.MatrixUDT()
         converter = sc._jvm.org.apache.spark.sql.catalyst.CatalystTypeConverters.createToCatalystConverter(matrix_udt)
@@ -35,4 +86,6 @@ class NdarrayConverter(object):
             converter.apply(dense_matrix), matrix_udt)
         return literal_matrix
 
-register_input_converter(NdarrayConverter(), prepend=True)
+
+register_input_converter(OneDimensionalDoubleNumpyArrayConverter(), prepend = True)
+register_input_converter(TwoDimensionalDoubleNumpyArrayConverter(), prepend = True)
--- python/glow/tests/test_conversions.py
+++ python/glow/tests/test_conversions.py
@@ -6,7 +6,7 @@ from pyspark.sql.types import StringType
 import pytest
 
 
-def test_convert(spark):
+def test_convert_matrix(spark):
     str_list = ['a', 'b']
     df = spark.createDataFrame(str_list, StringType())
     ndarray = np.array([[1.0, 2.1, 3.2], [4.3, 5.4, 6.5]])
@@ -16,13 +16,30 @@ def test_convert(spark):
     assert(output_rows[1].matrix == expected_matrix)
 
 
+def test_convert_array(spark):
+    str_list = ['a', 'b']
+    df = spark.createDataFrame(str_list, StringType())
+    ndarray = np.array([1.0, 2.1, 3.2])
+    output_rows = df.withColumn("array", lit(ndarray)).collect()
+    expected_array = [1.0, 2.1, 3.2]
+    assert(output_rows[0].array == expected_array)
+    assert(output_rows[1].array == expected_array)
+
+
 def test_convert_checks_dimension(spark):
+    # No support for 3-dimensional arrays
     ndarray = np.array([[[1.]]])
     with pytest.raises(Py4JJavaError):
         lit(ndarray)
 
 
-def test_convert_checks_type(spark):
+def test_convert_matrix_checks_type(spark):
     ndarray = np.array([[1, 2], [3, 4]])
+    with pytest.raises(AttributeError):
+        lit(ndarray)
+
+
+def test_convert_array_checks_type(spark):
+    ndarray = np.array([1, 2])
     with pytest.raises(AttributeError):
         lit(ndarray)
