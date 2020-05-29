66d554e4b6af19dca585d49925963c69ccb779b1
Karen Feng <karen.feng@databricks.com>
2020-05-29 11:36:18 -0700

    Support numpy literals (#213)
    * WIP
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Ndarray
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * More tests
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Support flat array
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Move registering
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * pytest
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Make docstring more accurate
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * idempotent registration
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Test fixup
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Move import out
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

---
+++ python/glow/tests/test_conversions.py
@@ -0,0 +1,62 @@
+from glow.conversions import OneDimensionalDoubleNumpyArrayConverter, TwoDimensionalDoubleNumpyArrayConverter
+from importlib import reload
+import numpy as np
+from py4j.protocol import Py4JJavaError
+from pyspark.ml.linalg import DenseMatrix
+from pyspark.sql.functions import lit
+from pyspark.sql.types import StringType
+import pytest
+
+
+def test_convert_matrix(spark):
+    str_list = ['a', 'b']
+    df = spark.createDataFrame(str_list, StringType())
+    ndarray = np.array([[1.0, 2.1, 3.2], [4.3, 5.4, 6.5]])
+    output_rows = df.withColumn("matrix", lit(ndarray)).collect()
+    expected_matrix = DenseMatrix(2, 3, [1.0, 2.1, 3.2, 4.3, 5.4, 6.5])
+    assert(output_rows[0].matrix == expected_matrix)
+    assert(output_rows[1].matrix == expected_matrix)
+
+
+def test_convert_array(spark):
+    str_list = ['a', 'b']
+    df = spark.createDataFrame(str_list, StringType())
+    ndarray = np.array([1.0, 2.1, 3.2])
+    output_rows = df.withColumn("array", lit(ndarray)).collect()
+    expected_array = [1.0, 2.1, 3.2]
+    assert(output_rows[0].array == expected_array)
+    assert(output_rows[1].array == expected_array)
+
+
+def test_convert_checks_dimension(spark):
+    # No support for 3-dimensional arrays
+    ndarray = np.array([[[1.]]])
+    with pytest.raises(Py4JJavaError):
+        lit(ndarray)
+
+
+def test_convert_matrix_checks_type(spark):
+    ndarray = np.array([[1, 2], [3, 4]])
+    with pytest.raises(AttributeError):
+        lit(ndarray)
+
+
+def test_convert_array_checks_type(spark):
+    ndarray = np.array([1, 2])
+    with pytest.raises(AttributeError):
+        lit(ndarray)
+
+
+def test_register_converters_idempotent(spark):
+    import glow.glow
+    for _ in range(3):
+        reload(glow.glow)
+        one_d_converters = 0
+        two_d_converters = 0
+        for c in spark._sc._gateway._gateway_client.converters:
+            if type(c) is OneDimensionalDoubleNumpyArrayConverter:
+                one_d_converters += 1
+            if type(c) is TwoDimensionalDoubleNumpyArrayConverter:
+                two_d_converters += 1
+        assert(one_d_converters == 1)
+        assert(two_d_converters == 1)
