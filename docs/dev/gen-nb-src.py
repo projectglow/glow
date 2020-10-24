185b1d075f11cf2b03af5a1d8a4580756b9dc958
Karen Feng <karen.feng@databricks.com>
2020-10-23 23:38:50 -0700

    more debug
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

--- docs/dev/gen-nb-src.py
+++ docs/dev/gen-nb-src.py
@@ -40,7 +40,8 @@ def main(html, cli_profile, workspace_tmp_dir):
 
     def run_cli_workspace_cmd(args):
         cmd = ['databricks', '--profile', cli_profile, 'workspace'] + args
-        subprocess.run(cmd, capture_output=True)
+        res = subprocess.run(cmd, capture_output=True)
+        raise ValueError(res)
         # subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 
     work_dir = os.path.join(workspace_tmp_dir, str(uuid.uuid4()))
