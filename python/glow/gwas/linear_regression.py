53df6ae56a6c2c2b2b46273be8f789396e05160f
Henry D <henrydavidge@gmail.com>
2020-11-02 15:53:21 -0500

    more tests
    Signed-off-by: Henry D <henrydavidge@gmail.com>

--- python/glow/gwas/linear_regression.py
+++ python/glow/gwas/linear_regression.py
@@ -6,33 +6,44 @@ from pyspark.sql import *
 from pyspark.sql.types import *
 from glow.functions import *
 import opt_einsum as oe
+from scipy import stats
 
 __all__ = ['linear_regression']
 
 def linear_regression(genotype_df, phenotype_df, covariate_df, fit_intercept=True, values_field='values'):
     result_fields = [
+        StructField('effect', DoubleType()),
+        StructField('standard_error', DoubleType()),
         StructField('tvalue', DoubleType()),
         StructField('pvalue', DoubleType()),
-        StructField('effect', DoubleType())
+        StructField('phenotype', StringType())
     ]
     fields = [field for field in genotype_df.schema.fields if field.name != values_field] + result_fields
     result_struct = StructType(fields)
     C = covariate_df.to_numpy('float64', copy=True)
     if fit_intercept:
-        C = np.hstack((np.ones(C.shape[0]), C))
-    Q = np.linalg.qr(C)[0]
+        intercept = np.ones((phenotype_df.shape[0], 1))
+        C = np.hstack((intercept, C)) if C.size else intercept
+    
     Y = phenotype_df.to_numpy('float64', copy=True)
     Y_mask = ~np.isnan(Y)
     Y[~Y_mask] = 0
+    Q = np.zeros((covariate_df.shape[0], covariate_df.shape[1], phenotype_df.shape[1]))
+    Q = np.linalg.qr(C)[0]
     QtY = Q.T @ Y
     YdotY = np.sum(Y * Y, axis = 0) - np.sum(QtY * QtY, axis = 0)
-    dof = c.shape[0] - c.shape[1] - 1
+    dof = C.shape[0] - C.shape[1] - 1
 
     def map_func(pdf_iterator):
         for pdf in pdf_iterator:
-            yield _linear_regression_inner(pdf, Y, Q, QtY, YdotY, Y_mask, dof)
+            yield _linear_regression_inner(pdf, Y, Q, QtY, YdotY, Y_mask, dof, phenotype_df.columns.to_series().astype('str'))
 
     return genotype_df.mapInPandas(map_func, result_struct)
+
+
+def _add_intercept(C, num_samples):
+    intercept = np.ones((num_samples, 1))
+    return np.hstack((intercept, C)) if C.size else intercept
 
 
 def _einsum(subscripts, *operands):
@@ -42,7 +53,7 @@ def _einsum(subscripts, *operands):
     return oe.contract(subscripts, *operands, memory_limit=5e7)
 
 
-def _linear_regression_inner(genotype_df, Y, Q, QtY, YdotY, Y_mask, dof):
+def _linear_regression_inner(genotype_df, Y, Q, QtY, YdotY, Y_mask, dof, phenotype_names):
     '''
     Applies a linear regression model to a block of genotypes.
     This is effectively a batch version of core/src/main/scala/io/projectglow/sql/expressions/LinearRegressionGwas.scala
@@ -61,11 +72,14 @@ def _linear_regression_inner(genotype_df, Y, Q, QtY, YdotY, Y_mask, dof):
     X = np.column_stack(genotype_df['values'].array)
 
     with oe.sharing.shared_intermediates():
-        XdotY = Y.T @ X - einsum('cp,sc,sg,sp->pg', QtY, Q, X, Y_mask)
-        XdotX_reciprocal = 1 / (einsum('sp,sg,sg->pg', Y_mask, X, X) -
-                                einsum('sc,sp,sg,sc,sp,sg->pg', Q, Y_mask, X, Q, Y_mask, X))
+        XdotY = Y.T @ X - _einsum('cp,sc,sg,sp->pg', QtY, Q, X, Y_mask)
+        XdotY = Y.T @ X - _einsum('cp,sc,sg,sp->pg', QtY, Q, X, Y_mask)
+        QtX = _einsum('sc,sp,sg->pgc', Q, Y_mask, X)
+        XdotX_reciprocal = 1 / (_einsum('sp,sg,sg->pg', Y_mask, X, X) -
+                                _einsum('pgc,pgc->pg', QtX, QtX))
 
     betas = XdotY * XdotX_reciprocal
+    QtX = _einsum('sc,sp,sg->pgc', Q, Y_mask, X)
     standard_error = np.sqrt((YdotY[:, None] * XdotX_reciprocal - betas * betas) / dof)
     T = betas / standard_error
     pvalues = 2 * stats.distributions.t.sf(np.abs(T), dof)
@@ -73,7 +87,9 @@ def _linear_regression_inner(genotype_df, Y, Q, QtY, YdotY, Y_mask, dof):
     del genotype_df['values']
     out_df = pd.concat([genotype_df] * Y.shape[1])
     out_df['effect'] = list(np.ravel(betas))
+    out_df['standard_error'] = list(np.ravel(standard_error))
     out_df['tvalue'] = list(np.ravel(T))
     out_df['pvalue'] = list(np.ravel(pvalues))
+    out_df['phenotype'] = phenotype_names.repeat(genotype_df.shape[0]).tolist()
 
     return out_df
--- python/glow/gwas/tests/test_linear_regression.py
+++ python/glow/gwas/tests/test_linear_regression.py
@@ -1,18 +1,144 @@
+import numpy as np
 import statsmodels.api as sm
+import pandas as pd
+import glow.gwas.linear_regression as lr
 
 def run_linear_regression(genotype_df, phenotype_df, covariate_df, fit_intercept=True):
+    phenotype_names = phenotype_df.columns.astype('str')
     C = covariate_df.to_numpy('float64', copy=True)
     if fit_intercept:
-        C = np.hstack((np.ones(C.shape[0]), C))
+        C = lr._add_intercept(C, genotype_df.shape[0])
+    if not C.size:
+        C = np.zeros((genotype_df.shape[0], 1))
     Q = np.linalg.qr(C)[0]
     Y = phenotype_df.to_numpy('float64', copy=True)
     Y_mask = ~np.isnan(Y)
     Y[~Y_mask] = 0
     QtY = Q.T @ Y
     YdotY = np.sum(Y * Y, axis = 0) - np.sum(QtY * QtY, axis = 0)
-    dof = c.shape[0] - c.shape[1] - 1
+    dof = C.shape[0] - C.shape[1] - 1
+    pdf = pd.DataFrame({'values': list(genotype_df.to_numpy().T)})
 
-    return _linear_regression_inner(pdf, Y, Q, QtY, YdotY, Y_mask, dof)
+    return lr._linear_regression_inner(pdf, Y, Q, QtY, YdotY, Y_mask, dof, phenotype_names)
+
+def residualize(Y, X):
+    pinv = np.linalg.pinv(X)
+    filled = np.nan_to_num(Y)
+    coef = pinv @ filled
+    print(coef)
+    pred = X @ coef
+    print(pred)
+    return Y - pred
 
 def statsmodels_baseline(genotype_df, phenotype_df, covariate_df, fit_intercept=True):
-    
+    # Project out covariates from genotypes and phenotypes
+    C = covariate_df.to_numpy('float64')
+    num_samples = C.shape[0] if C.shape else genotype_df.shape[0]
+    if fit_intercept:
+        C = lr._add_intercept(C, num_samples)
+    X = genotype_df.to_numpy('float64')
+    X = residualize(X, C)
+    Y = phenotype_df.to_numpy('float64')
+    print(Y)
+    Y = residualize(Y, C)
+    print(Y)
+    phenotype_df.columns = phenotype_df.columns.astype('str')
+    dof = C.shape[0] - C.shape[1] - 1
+    effects = []
+    errors = []
+    tvalues = []
+    pvalues = []
+    for phenotype in Y.T:
+        for genotype in X.T:
+            genotype = pd.Series(genotype, name='genotype')
+            model = sm.OLS(phenotype, genotype, missing='drop')
+            model.df_resid = dof
+            results = model.fit()
+            effects.append(results.params.genotype)
+            errors.append(results.bse.genotype)
+            tvalues.append(results.tvalues.genotype)
+            pvalues.append(results.pvalues.genotype)
+    return pd.DataFrame({
+        'effect': effects,
+        'standard_error': errors,
+        'tvalue': tvalues,
+        'pvalue': pvalues,
+        'phenotype': phenotype_df.columns.to_series().repeat(genotype_df.shape[1])})
+
+def regression_results_equal(df1, df2):
+    df1 = df1.sort_values('phenotype', kind='mergesort')
+    df2 = df2.sort_values('phenotype', kind='mergesort')
+    strings_equal = np.array_equal(df1.phenotype.array, df2.phenotype.array)
+    numerics_equal = np.allclose(df1.select_dtypes(exclude=['object']), df2.select_dtypes(exclude=['object']))
+    return strings_equal and numerics_equal
+
+def assert_glow_equals_golden(genotype_df, phenotype_df, covariate_df, fit_intercept=True):
+    glow = run_linear_regression(genotype_df, phenotype_df, covariate_df, fit_intercept)
+    golden = statsmodels_baseline(genotype_df, phenotype_df, covariate_df, fit_intercept)
+    assert regression_results_equal(glow, golden)
+
+def test_r_glm_baseline():
+    dataset = sm.datasets.get_rdataset('cars').data
+    genotype_df = dataset.loc[:, ['speed']]
+    phenotype_df = dataset.loc[:, ['dist']]
+    covariate_df = pd.DataFrame({})
+    expected = pd.DataFrame({'effect': [3.932409], 'standard_error': [0.415513],
+        'tvalue': [9.46399], 'pvalue': [1.489836e-12], 'phenotype': ['dist']})
+    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
+    assert regression_results_equal(expected, baseline)
+
+def test_r_glm_baseline_covariates():
+    dataset = sm.datasets.get_rdataset('cars').data
+    genotype_df = dataset.loc[:, ['speed']]
+    phenotype_df = dataset.loc[:, ['dist']]
+    covariate_df = pd.DataFrame({'intercept': np.ones(genotype_df.shape[0])})
+    expected = pd.DataFrame({'effect': [3.932409], 'standard_error': [0.415513],
+        'tvalue': [9.46399], 'pvalue': [1.489836e-12], 'phenotype': ['dist']})
+    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df, fit_intercept=False)
+    assert regression_results_equal(expected, baseline)
+
+
+def test_r_glm():
+    dataset = sm.datasets.get_rdataset('cars').data
+    genotype_df = dataset.loc[:, ['speed']]
+    phenotype_df = dataset.loc[:, ['dist']]
+    covariate_df = pd.DataFrame({})
+    assert_glow_equals_golden(genotype_df, phenotype_df, covariate_df)
+
+def test_r_glm_covariates():
+    dataset = sm.datasets.get_rdataset('cars').data
+    genotype_df = dataset.loc[:, ['speed']]
+    phenotype_df = dataset.loc[:, ['dist']]
+    covariate_df = pd.DataFrame({'intercept': np.ones(genotype_df.shape[0])})
+    assert_glow_equals_golden(genotype_df, phenotype_df, covariate_df, fit_intercept=False)
+
+def test_multiple():
+    num_samples = 100
+    genotype_df = pd.DataFrame(np.random.random((num_samples, 10)))
+    phenotype_df = pd.DataFrame(np.random.random((num_samples, 25)))
+    covariate_df = pd.DataFrame(np.random.random((num_samples, 5)))
+    assert_glow_equals_golden(genotype_df, phenotype_df, covariate_df)
+
+def test_missing():
+    num_samples = 5
+    genotype_df = pd.DataFrame(np.random.random((num_samples, 1)))
+    phenotype_df = pd.DataFrame(np.random.random((num_samples, 1)))
+    phenotype_df.loc[0, 0] = np.nan
+    covariate_df = pd.DataFrame(np.random.random((num_samples, 1)))
+    glow = run_linear_regression(genotype_df, phenotype_df, covariate_df)
+    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
+    assert regression_results_equal(glow, baseline)
+
+def test_multiple_spark(spark):
+    num_samples = 100
+    genotype_df = pd.DataFrame(np.random.random((num_samples, 10)))
+    phenotype_df = pd.DataFrame(np.random.random((num_samples, 25)))
+    covariate_df = pd.DataFrame(np.random.random((num_samples, 5)))
+    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
+    pdf = pd.DataFrame({'values': genotype_df.to_numpy().T.tolist()})
+    pdf['idx'] = pdf.index
+    results = (lr.linear_regression(spark.createDataFrame(pdf), phenotype_df, covariate_df)
+        .toPandas()
+        .sort_values(['idx'])
+        .drop('idx', axis=1))
+    assert regression_results_equal(baseline, results)
