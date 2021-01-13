f1c1d29634c96983b4bb6c0ee0edb1ae3fc19a5c
Karen Feng <karen.feng@databricks.com>
2021-01-13 09:39:56 -0800

    Approximate Firth correction (#318)
    * initial work
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * add file
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * workign score test
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * seems to work
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * continue
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * offset support; more tests
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * delete lin_reg.py
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * add docs, few more tests
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * add test file
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * fix last test
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * WIP
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * More work
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Fix docs, tests
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * memory limit
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * Correct param names
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * try explicitly broadcasting
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * Get it running
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Some clean up
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * update environment
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * undo explicit broadcast
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * fix typo
    
    Signed-off-by: Henry D <henrydavidge@gmail.com>
    
    * Clean up
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Replace penalized LL with deviance
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * log likelihood is a single num
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Fix stderr
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Do not correct vanilla score test
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * WIP
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Only binary level of residualization
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Values don't match
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Add y_mask
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Clean up
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Results from approx Firth don't match
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Tests match
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Clean up
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Clear warnings
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Working to match missing results
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Remove extra test
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * yapf
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Some cleanup
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Add test case where half of SNPs are corrected
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Add correction col
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Add golden logistf results
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * yapfAll
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Address comments
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * More clean up
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Add test for missing samples
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Address comments
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Remove correction_method
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Fix lin reg
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    * Set min spark vsn
    
    Signed-off-by: Karen Feng <karen.feng@databricks.com>
    
    Co-authored-by: Henry D <henrydavidge@gmail.com>

---
+++ python/glow/gwas/approx_firth.py
@@ -0,0 +1,197 @@
+from typing import Any, Optional
+import numpy as np
+from dataclasses import dataclass
+from typeguard import typechecked
+from nptyping import Float, NDArray
+from scipy import stats
+from statsmodels.base.model import Model
+import statsmodels.api as sm
+
+
+@dataclass
+class LogLikelihood:
+    pi: NDArray[(Any, ), Float]  # n_samples
+    I: NDArray[(Any, Any), Float]  # Fisher information matrix
+    deviance: Float  # -2 * penalized log likelihood
+
+
+@dataclass
+class FirthFit:
+    beta: NDArray[(Any, ), Float]  # n_covariates for null fit, 1 for SNP fit
+    log_likelihood: LogLikelihood
+
+
+@dataclass
+class FirthStatistics:
+    effect: Float
+    stderror: Float
+    tvalue: Float
+    pvalue: Float
+
+
+@typechecked
+def _calculate_log_likelihood(beta: NDArray[(Any, ), Float], model: Model) -> LogLikelihood:
+    pi = model.predict(beta)
+    I = -model.hessian(beta)
+    unpenalized_log_likelihood = model.loglike(beta)
+    _, log_abs_det = np.linalg.slogdet(I)
+    penalty = 0.5 * log_abs_det
+    deviance = -2 * (unpenalized_log_likelihood + penalty)
+    return LogLikelihood(pi, I, deviance)
+
+
+@typechecked
+def _fit_firth(beta_init: NDArray[(Any, ), Float],
+               X: NDArray[(Any, Any), Float],
+               y: NDArray[(Any, ), Float],
+               offset: NDArray[(Any, ), Float],
+               convergence_limit: float = 1e-5,
+               deviance_tolerance: float = 1e-6,
+               max_iter: int = 250,
+               max_step_size: int = 5,
+               max_half_steps: int = 25) -> Optional[FirthFit]:
+    '''
+    Firth’s bias-reduced penalized-likelihood logistic regression, based on the regenie implementation:
+    https://www.biorxiv.org/content/10.1101/2020.06.19.162354v2
+
+    Args:
+        beta_init : Initial beta values
+        X : Independent variable (covariate for null fit, genotype for SNP fit)
+        y : Dependent variable (phenotype)
+        offset : Phenotype offset only for null fit, phenotype + covariate effects for SNP fit
+        convergence_limit : Convergence is reached if all entries of the penalized score have smaller magnitude
+        deviance_tolerance : Non-inferiority margin when halving step size (default from regenie)
+        max_iter : Maximum number of Firth iterations (default from regenie)
+        max_step_size : Maximum step size during a Firth iteration (default from regenie)
+        max_half_steps : Maximum number of half-steps during a Firth iteration (default from regenie)
+
+    Returns:
+        None if the fit failed. Otherwise, a FirthFit object containing the fit information.
+    '''
+
+    n_iter = 0
+    beta = beta_init.copy()
+    model = sm.GLM(y, X, family=sm.families.Binomial(), offset=offset, missing='ignore')
+    log_likelihood = _calculate_log_likelihood(beta, model)
+
+    while n_iter < max_iter:
+        invI = np.linalg.pinv(log_likelihood.I)
+
+        # build hat matrix
+        rootG = np.sqrt(log_likelihood.pi * (1 - log_likelihood.pi))
+        rootG_X = rootG[:, None] * X  # equivalent to sqrt(diagflat(pi * (1 - pi))) @ X
+        h = np.diagonal(rootG_X @ invI @ rootG_X.T)
+
+        # modified score function
+        U = X.T @ (y - log_likelihood.pi + h * (0.5 - log_likelihood.pi))
+
+        # f' / f''
+        delta = invI @ U
+
+        # force absolute step size to be less than max_step_size for each entry of beta
+        mx = np.amax(np.abs(delta)) / max_step_size
+        if mx > 1:
+            delta /= mx
+
+        # if the penalized log likelihood decreased, recompute with step-halving
+        n_half_steps = 0
+        while n_half_steps < max_half_steps:
+            new_log_likelihood = _calculate_log_likelihood(beta + delta, model)
+            if new_log_likelihood.deviance < log_likelihood.deviance + deviance_tolerance:
+                break
+            delta /= 2
+            n_half_steps += 1
+
+        beta += delta
+        log_likelihood = new_log_likelihood
+
+        if np.amax(np.abs(U)) < convergence_limit:
+            break
+
+        n_iter += 1
+
+    if n_iter == max_iter:
+        # Failed to converge
+        return None
+
+    return FirthFit(beta, log_likelihood)
+
+
+@typechecked
+def perform_null_firth_fit(y: NDArray[(Any, ), Float], C: NDArray[(Any, Any), Float],
+                           mask: NDArray[(Any, ), bool], offset: Optional[NDArray[(Any, ), Float]],
+                           includes_intercept: bool) -> NDArray[(Any, ), Float]:
+    '''
+    Performs the null fit for approximate Firth in order to calculate the covariate effects to be
+    used as an offset during the SNP fits.
+
+    Args:
+        y : Dependent variable (phenotype)
+        C : Covariate matrix
+        mask : Missingness array; false if the sample is missing a phenotype value, true otherwise
+        offset : Phenotype offset
+        includes_intercept : True if the first column of the covariate matrix C represents an intercept term
+
+    Returns:
+        None if the Firth fit did not converge.
+        Otherwise, offset vector with per-sample covariate effects for SNP fits.
+    '''
+
+    firth_offset = np.zeros(y.shape)
+    offset = offset if offset is not None else np.zeros(y.shape)
+
+    masked_y = y[mask]
+    masked_C = C[mask, :]
+    masked_offset = offset[mask]
+
+    b0_null_fit = np.zeros(C.shape[1])
+    if includes_intercept:
+        b0_null_fit[0] = (0.5 + masked_y.sum()) / (mask.sum() + 1)
+        b0_null_fit[0] = np.log(b0_null_fit[0] / (1 - b0_null_fit[0])) - masked_offset.mean()
+    firth_fit_result = _fit_firth(b0_null_fit, masked_C, masked_y, masked_offset)
+    if firth_fit_result is None:
+        raise ValueError("Null fit failed!")
+    firth_offset[mask] = masked_offset + masked_C @ firth_fit_result.beta
+
+    return firth_offset
+
+
+# Skip typechecking for optimization
+def correct_approx_firth(x: NDArray[(Any, ), Float], y: NDArray[(Any, ), Float],
+                         firth_offset: NDArray[(Any, ), Float],
+                         mask: NDArray[(Any, ), bool]) -> Optional[FirthStatistics]:
+    '''
+    Calculate LRT statistics for a SNP using the approximate Firth method.
+
+    Args:
+        x : Genotypes for SNP
+        y : Dependent variable (phenotype)
+        offset : Phenotype offset including covariate effects from the null model fit
+        mask : Missingness array; false if the sample is missing a phenotype value, true otherwise
+
+    Returns:
+        None if the Firth fit did not converge.
+        Otherwise, likelihood-ratio test statistics.
+    '''
+
+    beta_init = np.zeros(1)
+    masked_y = y[mask]
+    masked_X = x[mask, None]
+    masked_offset = firth_offset[mask]
+    firth_fit = _fit_firth(beta_init, masked_X, masked_y, masked_offset)
+    if firth_fit is None:
+        return None
+
+    effect = firth_fit.beta.item()
+    # Likelihood-ratio test
+    null_model = sm.GLM(masked_y,
+                        masked_X,
+                        family=sm.families.Binomial(),
+                        offset=masked_offset,
+                        missing='ignore')
+    null_deviance = _calculate_log_likelihood(beta_init, null_model).deviance
+    tvalue = null_deviance - firth_fit.log_likelihood.deviance
+    pvalue = stats.chi2.sf(tvalue, 1)
+    # Based on the Hessian of the unpenalized log-likelihood
+    stderror = np.sqrt(np.linalg.pinv(firth_fit.log_likelihood.I).diagonal()[-1])
+    return FirthStatistics(effect=effect, stderror=stderror, tvalue=tvalue, pvalue=pvalue)
--- python/glow/gwas/log_reg.py
+++ python/glow/gwas/log_reg.py
@@ -1,4 +1,4 @@
-from pyspark.sql.types import ArrayType, StringType, StructField, DataType, StructType
+from pyspark.sql.types import ArrayType, BooleanType, StringType, StructField, DataType, StructType
 from typing import Any, Optional, Dict, Union
 import pandas as pd
 import numpy as np
@@ -10,28 +10,29 @@ from nptyping import Float, NDArray
 from scipy import stats
 import opt_einsum as oe
 from . import functions as gwas_fx
-from .functions import _OffsetType, _VALUES_COLUMN_NAME
+from . import approx_firth as af
+from .functions import _VALUES_COLUMN_NAME
 from ..wgr.functions import reshape_for_gwas
 
 __all__ = ['logistic_regression']
 
-fallback_none = 'none'
+correction_none = 'none'
+correction_approx_firth = 'approx-firth'
 
 
 @typechecked
-def logistic_regression(
-        genotype_df: DataFrame,
-        phenotype_df: pd.DataFrame,
-        covariate_df: pd.DataFrame = pd.DataFrame({}),
-        offset_df: pd.DataFrame = pd.DataFrame({}),
-        correction: str = 'none',  # TODO: Make approx-firth default
-        pvalue_threshold: float = 0.05,
-        fit_intercept: bool = True,
-        values_column: str = 'values',
-        dt: type = np.float64) -> DataFrame:
+def logistic_regression(genotype_df: DataFrame,
+                        phenotype_df: pd.DataFrame,
+                        covariate_df: pd.DataFrame = pd.DataFrame({}),
+                        offset_df: pd.DataFrame = pd.DataFrame({}),
+                        correction: str = correction_approx_firth,
+                        pvalue_threshold: float = 0.05,
+                        fit_intercept: bool = True,
+                        values_column: str = 'values',
+                        dt: type = np.float64) -> DataFrame:
     '''
     Uses logistic regression to test for association between genotypes and one or more binary
-    phenotypes. This is a distributed version of the method from regenie: 
+    phenotypes. This is a distributed version of the method from regenie:
     https://www.biorxiv.org/content/10.1101/2020.06.19.162354v2
 
     On the driver node, we fit a logistic regression model based on the covariates for each
@@ -57,7 +58,8 @@ def logistic_regression(
                     If two levels, the level 0 index should be the same as the ``phenotype_df``, and the level 1 index
                     should be the contig name. The two level index scheme allows for per-contig offsets like
                     LOCO predictions from GloWGR.
-        correction : Which test to use for variants that meet a significance threshold for the score test
+        correction : Which test to use for variants that meet a significance threshold for the score test. Supported
+                     methods are ``none`` and ``approx-firth``.
         pvalue_threshold : Variants with a pvalue below this threshold will be tested using the ``correction`` method.
         fit_intercept : Whether or not to add an intercept column to the covariate DataFrame
         values_column : A column name or column expression to test with linear regression. If a column name is provided,
@@ -69,6 +71,10 @@ def logistic_regression(
         A Spark DataFrame that contains
 
         - All columns from ``genotype_df`` except the ``values_column`` and the ``genotypes`` column if one exists
+        - ``effect``: The effect size (if approximate Firth correction was applied)
+        - ``stderror``: Standard error of the effect size (if approximate Firth correction was applied)
+        - ``correction_succeeded``: Whether the correction succeeded (if the correction test method is not ``none``).
+                                    True if succeeded, False if failed, null if correction was not applied.
         - ``tvalue``: The chi squared test statistic according to the score test or the correction method
         - ``pvalue``: P value estimated from the test statistic
         - ``phenotype``: The phenotype name as determiend by the column names of ``phenotype_df``
@@ -79,12 +85,23 @@ def logistic_regression(
     gwas_fx._validate_covariates_and_phenotypes(covariate_df, phenotype_df, is_binary=True)
     sql_type = gwas_fx._regression_sql_type(dt)
     genotype_df = gwas_fx._prepare_genotype_df(genotype_df, values_column, sql_type)
-    result_fields = [
-        # TODO: Probably want to put effect size and stderr here for approx-firth
+    base_result_fields = [
         StructField('tvalue', sql_type),
         StructField('pvalue', sql_type),
         StructField('phenotype', StringType())
     ]
+    if correction == correction_approx_firth:
+        result_fields = [
+            StructField('effect', sql_type),
+            StructField('stderror', sql_type),
+            StructField('correction_succeeded', BooleanType())
+        ] + base_result_fields
+    elif correction == correction_none:
+        result_fields = base_result_fields
+    else:
+        raise ValueError(
+            f"Only supported correction methods are '{correction_none}' and '{correction_approx_firth}'"
+        )
 
     result_struct = gwas_fx._output_schema(genotype_df.schema.fields, result_fields)
     C = covariate_df.to_numpy(dt, copy=True)
@@ -94,14 +111,20 @@ def logistic_regression(
     Y_mask = ~(np.isnan(Y))
     np.nan_to_num(Y, copy=False)
 
-    state = _create_log_reg_state(spark, phenotype_df, offset_df, sql_type, C)
+    if correction == correction_approx_firth:
+        Q = np.linalg.qr(C)[0]
+    else:
+        Q = None
+
+    state = _create_log_reg_state(spark, phenotype_df, offset_df, sql_type, C, correction,
+                                  fit_intercept)
 
     phenotype_names = phenotype_df.columns.to_series().astype('str')
 
     def map_func(pdf_iterator):
         for pdf in pdf_iterator:
-            yield gwas_fx._loco_dispatch(pdf, state, _logistic_regression_inner, C, Y_mask,
-                                         correction, phenotype_names)
+            yield gwas_fx._loco_dispatch(pdf, state, _logistic_regression_inner, C, Y, Y_mask, Q,
+                                         correction, pvalue_threshold, phenotype_names)
 
     return genotype_df.mapInPandas(map_func, result_struct)
 
@@ -111,6 +134,7 @@ class LogRegState:
     inv_CtGammaC: NDArray[(Any, Any, Any), Float]  # n_phenotypes x n_covariates x n_covariates
     gamma: NDArray[(Any, Any), Float]  # n_samples x n_phenotypes
     Y_res: NDArray[(Any, Any), Float]  # n_samples x n_phenotypes
+    firth_offset: Optional[NDArray[(Any, Any), Float]]  # n_samples x n_phenotypes
 
 
 def _logistic_null_model_predictions(y, X, mask, offset):
@@ -130,7 +154,8 @@ def _logistic_null_model_predictions(y, X, mask, offset):
     return remapped_predictions
 
 
-def _prepare_one_phenotype(C: NDArray[(Any, Any), Float], row: pd.Series) -> pd.Series:
+def _prepare_one_phenotype(C: NDArray[(Any, Any), Float], row: pd.Series, correction: str,
+                           includes_intercept: bool) -> pd.Series:
     '''
     Creates the broadcasted information for one (phenotype, offset) pair. The returned series
     contains the information eventually stored in a LogRegState.
@@ -140,7 +165,8 @@ def _prepare_one_phenotype(C: NDArray[(Any, Any), Float], row: pd.Series) -> pd.
     '''
     y = row['values']
     mask = ~np.isnan(y)
-    y_pred = _logistic_null_model_predictions(y, C, mask, row.get('offset'))
+    offset = row.get('offset')
+    y_pred = _logistic_null_model_predictions(y, C, mask, offset)
     y_res = np.nan_to_num(y - y_pred)
     gamma = y_pred * (1 - y_pred)
     CtGammaC = C.T @ (gamma[:, None] * C)
@@ -149,6 +175,9 @@ def _prepare_one_phenotype(C: NDArray[(Any, Any), Float], row: pd.Series) -> pd.
     row.drop(['values', 'offset'], inplace=True, errors='ignore')
     row['y_res'], row['gamma'], row['inv_CtGammaC'] = np.ravel(y_res), np.ravel(gamma), np.ravel(
         inv_CtGammaC)
+    if correction == correction_approx_firth:
+        row['firth_offset'] = np.ravel(
+            af.perform_null_firth_fit(y, C, mask, offset, includes_intercept))
     return row
 
 
@@ -164,14 +193,17 @@ def _pdf_to_log_reg_state(pdf: pd.DataFrame, phenotypes: pd.Series, n_covar: int
         phenotypes.size, n_covar, n_covar)
     gamma = np.column_stack(sorted_pdf['gamma'].array)
     Y_res = np.column_stack(sorted_pdf['y_res'].array)
-    return LogRegState(inv_CtGammaC, gamma, Y_res)
+    if 'firth_offset' in sorted_pdf:
+        firth_offset = np.column_stack(sorted_pdf['firth_offset'].array)
+    else:
+        firth_offset = None
+    return LogRegState(inv_CtGammaC, gamma, Y_res, firth_offset)
 
 
 @typechecked
-def _create_log_reg_state(
-        spark: SparkSession, phenotype_df: pd.DataFrame, offset_df: pd.DataFrame,
-        sql_type: DataType, C: NDArray[(Any, Any),
-                                       Float]) -> Union[LogRegState, Dict[str, LogRegState]]:
+def _create_log_reg_state(spark: SparkSession, phenotype_df: pd.DataFrame, offset_df: pd.DataFrame,
+                          sql_type: DataType, C: NDArray[(Any, Any), Float], correction: str,
+                          fit_intercept: bool) -> Union[LogRegState, Dict[str, LogRegState]]:
     '''
     Creates the broadcasted LogRegState object (or one object per contig if LOCO offsets were provided).
 
@@ -186,6 +218,10 @@ def _create_log_reg_state(
         StructField('gamma', ArrayType(sql_type)),
         StructField('inv_CtGammaC', ArrayType(sql_type))
     ]
+
+    if correction == correction_approx_firth:
+        result_fields.append(StructField('firth_offset', ArrayType(sql_type)))
+
     if offset_type == gwas_fx._OffsetType.NO_OFFSET:
         df = pivoted_phenotype_df
     else:
@@ -197,7 +233,7 @@ def _create_log_reg_state(
 
     def map_func(pdf_iterator):
         for pdf in pdf_iterator:
-            yield pdf.apply(lambda r: _prepare_one_phenotype(C, r),
+            yield pdf.apply(lambda r: _prepare_one_phenotype(C, r, correction, fit_intercept),
                             axis='columns',
                             result_type='expand')
 
@@ -217,7 +253,7 @@ def _create_log_reg_state(
 
 
 def _logistic_residualize(X: NDArray[(Any, Any), Float], C: NDArray[(Any, Any), Float],
-                          Y_mask: NDArray[(Any, Any), Float], gamma: NDArray[(Any, Any), Float],
+                          Y_mask: NDArray[(Any, Any), bool], gamma: NDArray[(Any, Any), Float],
                           inv_CtGammaC: NDArray[(Any, Any), Float]) -> NDArray[(Any, Any), Float]:
     '''
     Residualize the genotype vectors given the null model predictions.
@@ -228,8 +264,11 @@ def _logistic_residualize(X: NDArray[(Any, Any), Float], C: NDArray[(Any, Any), 
 
 
 def _logistic_regression_inner(genotype_pdf: pd.DataFrame, log_reg_state: LogRegState,
-                               C: NDArray[(Any, Any), Float], Y_mask: NDArray[(Any, Any), Float],
-                               fallback_method: str, phenotype_names: pd.Series) -> pd.DataFrame:
+                               C: NDArray[(Any, Any), Float], Y: NDArray[(Any, Any), Float],
+                               Y_mask: NDArray[(Any, Any),
+                                               bool], Q: Optional[NDArray[(Any, Any),
+                                                                          Float]], correction: str,
+                               pvalue_threshold: float, phenotype_names: pd.Series) -> pd.DataFrame:
     '''
     Tests a block of genotypes for association with binary traits. We first residualize
     the genotypes based on the null model fit, then perform a fast score test to check for
@@ -242,6 +281,11 @@ def _logistic_regression_inner(genotype_pdf: pd.DataFrame, log_reg_state: LogReg
     c, d: covariate
     '''
     X = np.column_stack(genotype_pdf[_VALUES_COLUMN_NAME].array)
+
+    # For approximate Firth correction, we perform a linear residualization
+    if correction == correction_approx_firth:
+        X = gwas_fx._residualize_in_place(X, Q)
+
     with oe.shared_intermediates():
         X_res = _logistic_residualize(X, C, Y_mask, log_reg_state.gamma, log_reg_state.inv_CtGammaC)
         num = gwas_fx._einsum('sgp,sp->pg', X_res, log_reg_state.Y_res)**2
@@ -249,13 +293,31 @@ def _logistic_regression_inner(genotype_pdf: pd.DataFrame, log_reg_state: LogReg
     t_values = np.ravel(num / denom)
     p_values = stats.chi2.sf(t_values, 1)
 
-    if fallback_method != fallback_none:
-        # TODO: Call approx firth here
-        ()
-
     del genotype_pdf[_VALUES_COLUMN_NAME]
     out_df = pd.concat([genotype_pdf] * log_reg_state.Y_res.shape[1])
     out_df['tvalue'] = list(np.ravel(t_values))
     out_df['pvalue'] = list(np.ravel(p_values))
     out_df['phenotype'] = phenotype_names.repeat(genotype_pdf.shape[0]).tolist()
+
+    if correction != correction_none:
+        out_df['correction_succeeded'] = None
+        correction_indices = list(np.where(out_df['pvalue'] < pvalue_threshold)[0])
+        if correction == correction_approx_firth:
+            out_df['effect'] = np.nan
+            out_df['stderror'] = np.nan
+            for correction_idx in correction_indices:
+                snp_idx = correction_idx % X.shape[1]
+                pheno_idx = int(correction_idx / X.shape[1])
+                approx_firth_snp_fit = af.correct_approx_firth(
+                    X[:, snp_idx], Y[:, pheno_idx], log_reg_state.firth_offset[:, pheno_idx],
+                    Y_mask[:, pheno_idx])
+                if approx_firth_snp_fit is None:
+                    out_df.correction_succeeded.iloc[correction_idx] = False
+                else:
+                    out_df.correction_succeeded.iloc[correction_idx] = True
+                    out_df.effect.iloc[correction_idx] = approx_firth_snp_fit.effect
+                    out_df.stderror.iloc[correction_idx] = approx_firth_snp_fit.stderror
+                    out_df.tvalue.iloc[correction_idx] = approx_firth_snp_fit.tvalue
+                    out_df.pvalue.iloc[correction_idx] = approx_firth_snp_fit.pvalue
+
     return out_df
