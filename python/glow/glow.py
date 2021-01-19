d7d020675899447a217c5d6868ddcc8263f1cc13
Henry D <henrydavidge@gmail.com>
2021-01-19 13:41:09 -0500

    comment
    Signed-off-by: Henry D <henrydavidge@gmail.com>

--- python/glow/glow.py
+++ python/glow/glow.py
@@ -67,7 +67,8 @@ def register(session: SparkSession, new_session: bool=True) -> SparkSession:
         session: Spark session
         new_session: If ``True``, create a new Spark using ``session.newSession()`` before registering
                      extensions. This may be necessary if you're using functions that register new
-                     analysis rules.
+                     analysis rules. The new session will have isolated UDFs, configurations, and temporary tables,
+                     but shares the existing ``SparkContext`` and cached data.
 
     Example:
         >>> import glow
