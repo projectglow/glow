cfc0b23611c75b7f9f8e480f8d780f05800454b2
brian cajes <brian@empiricotx.com>
2021-05-13 13:44:26 -0700

    init prototype for exposing n, sum_x, and y_transpose_x, as is done in hail
    Signed-off-by: brian cajes <brian@empiricotx.com>

--- python/glow/gwas/lin_reg.py
+++ python/glow/gwas/lin_reg.py
@@ -4,7 +4,7 @@ from nptyping import Float, NDArray
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Union
 from pyspark.sql import Column, DataFrame
-from pyspark.sql.types import StringType, StructField, IntegerType
+from pyspark.sql.types import StringType, StructField
 from scipy import stats
 from typeguard import typechecked
 from . import functions as gwas_fx
@@ -22,12 +22,11 @@ def linear_regression(genotype_df: DataFrame,
                       contigs: Optional[List[str]] = None,
                       add_intercept: bool = True,
                       values_column: Union[str, Column] = 'values',
-                      dt: type = np.float64,
-                      verbose_output: bool = False) -> DataFrame:
+                      dt: type = np.float64) -> DataFrame:
     '''
     Uses linear regression to test for association between genotypes and one or more phenotypes.
     The implementation is a distributed version of the method used in regenie: 
-    https://www.nature.com/articles/s41588-021-00870-7
+    https://www.biorxiv.org/content/10.1101/2020.06.19.162354v2
 
     Implementation details:
 
@@ -79,10 +78,6 @@ def linear_regression(genotype_df: DataFrame,
                         ``genotype_df`` should have a column with this name and a numeric array type. If a column
                         expression is provided, the expression should return a numeric array type.
         dt : The numpy datatype to use in the linear regression test. Must be ``np.float32`` or ``np.float64``.
-        verbose_output: Whether or not to generate additional test statistics (n, sum_x, y_transpose_x)
-                        to the output DataFrame.  These values are derived directly from phenotype_df and genotype_df,
-                        and does not reflect any standardization performed as part of the implementation of
-                        linear_regression.
 
     Returns:
         A Spark DataFrame that contains
@@ -93,10 +88,6 @@ def linear_regression(genotype_df: DataFrame,
         - ``tvalue``: The T statistic
         - ``pvalue``: P value estimated from a two sided T-test
         - ``phenotype``: The phenotype name as determined by the column names of ``phenotype_df``
-        - ``n``(int): (verbose_output only) number of samples with non-null phenotype
-        - ``sum_x``(float): (verbose_output only) sum of genotype inputs
-        - ``y_transpose_x``(float): (verbose_output only) dot product of phenotype response (missing values encoded as zeros)
-                             and genotype input, i.e. phenotype value * number of alternate alleles
     '''
 
     gwas_fx._check_spark_version(genotype_df.sql_ctx.sparkSession)
@@ -107,6 +98,16 @@ def linear_regression(genotype_df: DataFrame,
 
     genotype_df = gwas_fx._prepare_genotype_df(genotype_df, values_column, sql_type)
 
+    # Construct output schema
+    result_fields = [
+        StructField('effect', sql_type),
+        StructField('stderror', sql_type),
+        StructField('tvalue', sql_type),
+        StructField('pvalue', sql_type),
+        StructField('phenotype', StringType())
+    ]
+    result_struct = gwas_fx._output_schema(genotype_df.schema.fields, result_fields)
+
     C = covariate_df.to_numpy(dt, copy=True)
     if add_intercept:
         C = gwas_fx._add_intercept(C, phenotype_df.shape[0])
@@ -116,7 +117,7 @@ def linear_regression(genotype_df: DataFrame,
     Y = phenotype_df.to_numpy(dt, copy=True)
     Y_mask = (~np.isnan(Y)).astype(dt)
     Y = np.nan_to_num(Y, copy=False)
-    Y_for_verbose_output = np.copy(Y) if verbose_output else None
+    Y_raw_nan_filled = np.nan_to_num(Y, copy=True)
     Y -= Y.mean(axis=0)  # Mean-center
     Y = gwas_fx._residualize_in_place(Y, Q) * Y_mask  # Residualize
     Y_scale = np.sqrt(np.sum(Y**2, axis=0) / (Y_mask.sum(axis=0) - Q.shape[1]))
@@ -126,8 +127,13 @@ def linear_regression(genotype_df: DataFrame,
 
     dof = C.shape[0] - C.shape[1] - 1
 
-    return _generate_linreg_output(genotype_df, sql_type, Y_state, Y_mask, Y_scale, Q, dof,
-                                   phenotype_df, Y_for_verbose_output, verbose_output)
+    def map_func(pdf_iterator):
+        for pdf in pdf_iterator:
+            yield gwas_fx._loco_dispatch(pdf, Y_state, _linear_regression_inner, Y_mask, Y_scale, Q,
+                                         dof,
+                                         phenotype_df.columns.to_series().astype('str'), Y_raw_nan_filled)
+
+    return genotype_df.mapInPandas(map_func, result_struct)
 
 
 @dataclass
@@ -168,9 +174,8 @@ def _create_one_YState(Y: NDArray[(Any, Any), Float], phenotype_df: pd.DataFrame
 # @typechecked -- typeguard does not support numpy array
 def _linear_regression_inner(genotype_pdf: pd.DataFrame, Y_state: YState,
                              Y_mask: NDArray[(Any, Any), Float], Y_scale: NDArray[(Any, ), Float],
-                             Q: NDArray[(Any, Any), Float], dof: int, phenotype_names: pd.Series,
-                             Y_raw: Optional[NDArray[(Any, Any), Float]],
-                             verbose_output: Optional[bool]) -> pd.DataFrame:
+                             Q: NDArray[(Any, Any), Float], dof: int,
+                             phenotype_names: pd.Series, Y_raw: NDArray[(Any, Any), Float]) -> pd.DataFrame:
     '''
     Applies a linear regression model to a block of genotypes. We first project the covariates out of the
     genotype block and then perform single variate linear regression for each site.
@@ -185,15 +190,9 @@ def _linear_regression_inner(genotype_pdf: pd.DataFrame, Y_state: YState,
 
     So, if a matrix's indices are `sg` (like the X matrix), it has one row per sample and one column per genotype.
     '''
-
     X = np.column_stack(genotype_pdf[_VALUES_COLUMN_NAME].array)
-    del genotype_pdf[_VALUES_COLUMN_NAME]
-    num_genotypes = genotype_pdf.shape[0]
-    out_df = pd.concat([genotype_pdf] * Y_state.Y.shape[1])
-    if verbose_output:
-        out_df["n"] = list(Y_mask.sum(axis=0).repeat(num_genotypes))
-        out_df["sum_x"] = list(np.ravel(Y_mask.T @ X))
-        out_df["y_transpose_x"] = list(np.ravel(Y_raw.T @ X))
+    sum_x = Y_mask.T @ X #rows indexed by phe, cols indexed by variants
+    y_transpose_x = Y_raw.T @ X
     X = gwas_fx._residualize_in_place(X, Q)
 
     XdotY = Y_state.Y.T @ X
@@ -203,6 +202,10 @@ def _linear_regression_inner(genotype_pdf: pd.DataFrame, Y_state: YState,
     T = betas / standard_error
     pvalues = 2 * stats.distributions.t.sf(np.abs(T), dof)
 
+    del genotype_pdf[_VALUES_COLUMN_NAME]
+
+    num_genotypes = genotype_pdf.shape[0]
+    out_df = pd.concat([genotype_pdf] * Y_state.Y.shape[1])
     Y_scale_mat = Y_scale[:, None]
     out_df['effect'] = list(np.ravel(betas * Y_scale_mat))
     out_df['stderror'] = list(np.ravel(standard_error * Y_scale_mat))
@@ -217,33 +220,3 @@ def _linear_regression_inner(genotype_pdf: pd.DataFrame, Y_state: YState,
     pdb.set_trace_remote()
 
     return out_df
-
-
-def _generate_linreg_output(genotype_df, sql_type, Y_state, Y_mask, Y_scale, Q, dof, phenotype_df,
-                            Y_for_verbose_output, verbose_output) -> DataFrame:
-    # Construct output schema
-    result_fields = [
-        StructField('effect', sql_type),
-        StructField('stderror', sql_type),
-        StructField('tvalue', sql_type),
-        StructField('pvalue', sql_type),
-        StructField('phenotype', StringType())
-    ]
-
-    if verbose_output:
-        result_fields += ([
-            StructField('n', IntegerType()),
-            StructField('sum_x', sql_type),
-            StructField('y_transpose_x', sql_type)
-        ])
-
-    result_struct = gwas_fx._output_schema(genotype_df.schema.fields, result_fields)
-
-    def map_func(pdf_iterator):
-        for pdf in pdf_iterator:
-            yield gwas_fx._loco_dispatch(pdf, Y_state, _linear_regression_inner, Y_mask, Y_scale, Q,
-                                         dof,
-                                         phenotype_df.columns.to_series().astype('str'),
-                                         Y_for_verbose_output, verbose_output)
-
-    return genotype_df.mapInPandas(map_func, result_struct)
--- python/glow/gwas/tests/test_lin_reg.py
+++ python/glow/gwas/tests/test_lin_reg.py
@@ -26,7 +26,7 @@ def run_linear_regression(genotype_df, phenotype_df, covariate_df, add_intercept
     pdf = pd.DataFrame({lr._VALUES_COLUMN_NAME: list(genotype_df.to_numpy('float64').T)})
 
     return lr._linear_regression_inner(pdf, Y_state, Y_mask.astype('float64'), Y_scale, Q, dof,
-                                       phenotype_names, None, None)
+                                       phenotype_names)
 
 
 def run_linear_regression_spark(spark,
@@ -225,132 +225,14 @@ def test_sum_(spark, rg):
     baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
     assert regression_results_equal(glow, baseline)
 
-
 @pytest.mark.min_spark('3')
 def test_verbose_output(spark, rg):
-    num_samples = 10
-    genotype_df = pd.DataFrame(rg.random((num_samples, 1)))
-    phenotype_df = pd.DataFrame(rg.random((num_samples, 3)))
-    phenotype_df.loc[0, 0] = np.nan
-    phenotype_df.loc[[1, 3, 5], 1] = np.nan
-    covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark, genotype_df, phenotype_df, covariate_df, verbose_output = True)
-    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n","sum_x","y_transpose_x"]), baseline)
-    assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(), np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(), (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
-
-@pytest.mark.min_spark('3')
-def test_verbose_output(spark, rg):
-    num_samples = 10
-    genotype_df = pd.DataFrame(rg.random((num_samples, 1)))
-    phenotype_df = pd.DataFrame(rg.random((num_samples, 3)))
-    phenotype_df.loc[0, 0] = np.nan
-    phenotype_df.loc[[1, 3, 5], 1] = np.nan
-    covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark,
-                                       genotype_df,
-                                       phenotype_df,
-                                       covariate_df,
-                                       verbose_output=True)
-    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n", "sum_x", "y_transpose_x"]), baseline)
-    assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(),
-                       np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(),
-                       (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
-
-
-@pytest.mark.min_spark('3')
-def test_verbose_output(spark, rg):
-    num_samples = 10
-    genotype_df = pd.DataFrame(rg.random((num_samples, 1)))
-    phenotype_df = pd.DataFrame(rg.random((num_samples, 3)))
-    phenotype_df.loc[0, 0] = np.nan
-    phenotype_df.loc[[1, 3, 5], 1] = np.nan
-    covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark, genotype_df, phenotype_df, covariate_df, verbose_output = True)
-    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n","sum_x","y_transpose_x"]), baseline)
-    assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(), np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(), (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
-
-@pytest.mark.min_spark('3')
-def test_verbose_output(spark, rg):
-    num_samples = 10
-    genotype_df = pd.DataFrame(rg.random((num_samples, 1)))
-    phenotype_df = pd.DataFrame(rg.random((num_samples, 3)))
-    phenotype_df.loc[0, 0] = np.nan
-    phenotype_df.loc[[1, 3, 5], 1] = np.nan
-    covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark, genotype_df, phenotype_df, covariate_df, verbose_output = True)
-    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n","sum_x","y_transpose_x"]), baseline)
-    assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(), np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(), (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
-
-@pytest.mark.min_spark('3')
-def test_verbose_output(spark, rg):
-    num_samples = 10
-    genotype_df = pd.DataFrame(rg.random((num_samples, 1)))
-    phenotype_df = pd.DataFrame(rg.random((num_samples, 3)))
-    phenotype_df.loc[0, 0] = np.nan
-    phenotype_df.loc[[1, 3, 5], 1] = np.nan
-    covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark,
-                                       genotype_df,
-                                       phenotype_df,
-                                       covariate_df,
-                                       verbose_output=True)
-    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n", "sum_x", "y_transpose_x"]), baseline)
-    assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(),
-                       np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(),
-                       (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
-
-
-@pytest.mark.min_spark('3')
-def test_verbose_output(spark, rg):
-    num_samples = 10
-    genotype_df = pd.DataFrame(rg.random((num_samples, 1)))
-    phenotype_df = pd.DataFrame(rg.random((num_samples, 3)))
-    phenotype_df.loc[0, 0] = np.nan
-    phenotype_df.loc[[1, 3, 5], 1] = np.nan
-    covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark, genotype_df, phenotype_df, covariate_df, verbose_output = True)
-    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n","sum_x","y_transpose_x"]), baseline)
-    assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(), np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(), (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
-
-@pytest.mark.min_spark('3')
-def test_verbose_output(spark, rg):
-    num_samples = 10
-    genotype_df = pd.DataFrame(rg.random((num_samples, 1)))
-    phenotype_df = pd.DataFrame(rg.random((num_samples, 3)))
-    phenotype_df.loc[0, 0] = np.nan
-    phenotype_df.loc[[1, 3, 5], 1] = np.nan
-    covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark,
-                                       genotype_df,
-                                       phenotype_df,
-                                       covariate_df,
-                                       verbose_output=True)
-    baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n", "sum_x", "y_transpose_x"]), baseline)
-    assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(),
-                       np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(),
-                       (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
-
+    num_samples = 5
+    genotype_df =  pd.DataFrame({'rs0': [0, 0, 0, 0, 0], 'rs1': [2,2,1,1,1], 'rs2': [1,0,0,1,1], 'rs3': [1,2,0,1,1]})
+    phenotype_df = pd.DataFrame({'phe1': [np.nan, 1, 1, 1, 1], 'phe2': [np.nan,1,0,0,0], 'phe3': [1,0,1,0,1]})
+    covariate_df = pd.DataFrame(rg.random((num_samples, 2)))
+    glow = run_linear_regression_spark(spark, genotype_df, phenotype_df, covariate_df)
+    #TODO complete assertions
 
 @pytest.mark.min_spark('3')
 def test_multiple_spark(spark, rg):
