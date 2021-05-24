af8bb0c7d5dcb97fefb567b6d8f9bd14668d91d3
Karen Feng <karen.feng@databricks.com>
2021-05-24 15:31:29 -0700

    Base code
    Signed-off-by: Karen Feng <karen.feng@databricks.com>

---
+++ docs/dev/run-nb-test.py
@@ -0,0 +1,81 @@
+'''
+Runs a Glow notebook in Databricks as an integration test.
+
+Before running this, configure your Databricks CLI profile.
+
+Example usage:
+  python3 docs/dev/run-nb-test.py
+'''
+import click
+from datetime import datetime
+import glob
+import json
+import subprocess
+import time
+import os
+import uuid
+
+SOURCE_DIR = 'docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE'
+CLUSTER_JSON = 'docs/dev/cluster-config.json'
+SOURCE_EXTS = ['scala', 'py', 'r', 'sql']
+
+
+def run_cli_cmd(cli_profile, api, args):
+    cmd = ['databricks', '--profile', cli_profile, api] + args
+    # print(f"Running command {cmd}")
+    res = subprocess.run(cmd, capture_output=True)
+    if res.returncode is not 0:
+        raise ValueError(res)
+    return res.stdout
+
+
+@click.command()
+@click.option('--cli-profile', default='DEFAULT', help='Databricks CLI profile name.')
+@click.option('--workspace-tmp-dir', default='/tmp/glow-nb-test-ci', help='Base workspace dir for import and testing.')
+def main(cli_profile, workspace_tmp_dir):
+    identifier = str(uuid.uuid4())
+    work_dir = os.path.join(workspace_tmp_dir, identifier)
+    with open('docs/dev/jobs-config.json', 'r') as f:
+        jobs_json = json.load(f)
+
+    nbs = [os.path.relpath(path, SOURCE_DIR).split('.')[0]
+           for path in glob.glob(SOURCE_DIR + '/**', recursive=True)
+           if not os.path.isdir(path)]
+    nb_to_run_id = {}
+
+    try:
+        print(f"Importing source files from {SOURCE_DIR} to {work_dir}")
+        run_cli_cmd(cli_profile, 'workspace', ['mkdirs', work_dir])
+        run_cli_cmd(cli_profile, 'workspace', ['import_dir', SOURCE_DIR, work_dir])
+
+        for nb in nbs:
+            print(f"Launching run for notebook {nb}")
+            jobs_json['name'] = 'Glow notebook integration test - ' + nb
+            jobs_json['notebook_task'] = {'notebook_path': work_dir + '/' + nb}
+            run_submit = run_cli_cmd(cli_profile, 'runs', ['submit', '--json', json.dumps(jobs_json)])
+            run_id = json.loads(run_submit)['run_id']
+            print(f"Run ID is {run_id}")
+            nb_to_run_id[nb] = str(run_id)
+            break
+    finally:
+        nb_to_run_state = {}
+        while True:
+            print(f"=== Status check at {datetime.now().strftime('%H:%M:%S')} ===")
+            for nb, run_id in nb_to_run_id.items():
+                run_get = run_cli_cmd(cli_profile, 'runs', ['get', '--run-id', run_id])
+                run_state = json.loads(run_get)['state']
+                nb_to_run_state[nb] = run_state
+            if all([run_state['life_cycle_state'] == 'TERMINATED' for run_state in nb_to_run_state.values()]):
+                print("All jobs are terminated!")
+                for nb, run_state in nb_to_run_state.items():
+                    print(f"{nb}: {run_state['result_state']}")
+                break
+            else:
+                print("Not all jobs are terminated.")
+                for nb, run_state in nb_to_run_state.items():
+                    print(f"{nb}: {run_state['life_cycle_state']} {run_state['state_message']}")
+            time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
