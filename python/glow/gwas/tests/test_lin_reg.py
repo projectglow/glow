8d7c23054af3bf1a1159d16f11f87ff0706c5b45
brian cajes <brian@empiricotx.com>
2021-06-04 13:20:47 -0700

    ran python/yapfAll
    Signed-off-by: brian cajes <brian@empiricotx.com>

--- python/glow/gwas/tests/test_lin_reg.py
+++ python/glow/gwas/tests/test_lin_reg.py
@@ -222,12 +222,19 @@ def test_verbose_output(spark, rg):
     phenotype_df.loc[0, 0] = np.nan
     phenotype_df.loc[[1, 3, 5], 1] = np.nan
     covariate_df = pd.DataFrame(rg.random((num_samples, 3)))
-    glow = run_linear_regression_spark(spark, genotype_df, phenotype_df, covariate_df, verbose_output = True)
+    glow = run_linear_regression_spark(spark,
+                                       genotype_df,
+                                       phenotype_df,
+                                       covariate_df,
+                                       verbose_output=True)
     baseline = statsmodels_baseline(genotype_df, phenotype_df, covariate_df)
-    assert regression_results_equal(glow.drop(columns=["n","sum_x","y_transpose_x"]), baseline)
+    assert regression_results_equal(glow.drop(columns=["n", "sum_x", "y_transpose_x"]), baseline)
     assert glow.n.to_list() == [9, 7, 10]
-    assert np.allclose(glow.y_transpose_x.to_numpy(), np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
-    assert np.allclose(glow.sum_x.to_numpy(), (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
+    assert np.allclose(glow.y_transpose_x.to_numpy(),
+                       np.nan_to_num(phenotype_df.to_numpy().T) @ genotype_df[0].array)
+    assert np.allclose(glow.sum_x.to_numpy(),
+                       (~np.isnan(phenotype_df.to_numpy())).T @ genotype_df[0].array)
+
 
 @pytest.mark.min_spark('3')
 def test_verbose_output(spark, rg):
