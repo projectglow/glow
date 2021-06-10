accd925d8e27f97167474d7adef88089676499ed
William Brandler <william.brandler@databricks.com>
2021-06-10 12:10:40 -0700

    remove old notebooks
    Signed-off-by: wbrandler <william.brandler@databricks.com>
    Signed-off-by: William Brandler <William.Brandler@databricks.com>

--- docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE/readme/2d_install_deep_variant.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC ##<img src="https://databricks.com/wp-content/themes/databricks/assets/images/databricks-logo.png" alt="logo" width="240"/> 
-# MAGIC ### Genomics Technical Guide
-# MAGIC 
-# MAGIC ##### Install Deep Variant
-# MAGIC 
-# MAGIC _coming soon!_
