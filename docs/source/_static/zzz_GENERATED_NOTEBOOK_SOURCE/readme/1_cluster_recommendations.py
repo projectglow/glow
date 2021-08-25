07aa1878fa3bf2af196e5cbe6719f7a7040ce9d7
Alex Barreto <axbarreto@lavabit.com>
2021-08-24 22:15:26 -0400

    update hail
    Signed-off-by: wbrandler <william.brandler@databricks.com>

---
+++ docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE/readme/1_cluster_recommendations.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##<img src="https://databricks.com/wp-content/themes/databricks/assets/images/databricks-logo.png" alt="logo" width="240"/> 
+# MAGIC ### <img src="https://databricks-knowledge-repo-images.s3.us-east-2.amazonaws.com/HLS/glow/project_glow_logo.png" alt="logo" width="35"/> Glow Technical Guide
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### 1. Cluster recommendations
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 1. For ingest of variant data, use compute optimized instances
+# MAGIC 2. For querying, use delta cache accelerated instances
+# MAGIC 3. For genetic association studies, use memory optimized instances (more performant per core hour)
+# MAGIC   - Amazon Web Services (AWS): r5d series
+# MAGIC   - Microsoft Azure: E8s series
+# MAGIC   - Google Cloud Platform (GCP): _coming soon!_
