b652e558f8e8641dd1f330cbf1d014169894a260
William Brandler <William.Brandler@databricks.com>
2021-10-05 10:56:05 -0700

    remove init script
    Signed-off-by: William Brandler <William.Brandler@databricks.com>

--- docs/dev/init-scripts/htslib_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sleep 10
-wget https://github.com/samtools/htslib/releases/download/1.9/htslib-1.9.tar.bz2
-tar xjvf htslib-1.9.tar.bz2
-cd htslib-1.9
-./configure
-make
-make install
-sleep 10
