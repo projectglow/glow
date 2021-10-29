cee1240867407ba6b7ac4c42a54c298311fb26f6
William Brandler <William.Brandler@databricks.com>
2021-10-28 17:03:39 -0700

    add notebooks for quickstart gwas tutorial
    Signed-off-by: William Brandler <William.Brandler@databricks.com>

---
+++ docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE/tertiary/1_quickstart_quality_control.py
@@ -0,0 +1,173 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC #### Quality control to prepare delta table for GWAS
+# MAGIC 
+# MAGIC By running glow transform functions `split_multiallelics`, `mean_substitute`, and `genotype_states`
+# MAGIC 
+# MAGIC Then filter,
+# MAGIC 
+# MAGIC 1. monomorphic variants using `array_distinct`
+# MAGIC 2. allele frequency
+# MAGIC 3. hardy weinberg equilibrium
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### adjust spark confs
+# MAGIC 
+# MAGIC see [split-multiallelics](https://glow.readthedocs.io/en/latest/etl/variant-splitter.html#split-multiallelics) docs
+
+# COMMAND ----------
+
+spark.conf.set("spark.sql.codegen.wholeStage", False)
+
+# COMMAND ----------
+
+import pyspark.sql.functions as fx
+from pyspark.sql.types import *
+import glow
+spark = glow.register(spark)
+
+import pandas as pd
+import numpy as np
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ##### Helper functions
+
+# COMMAND ----------
+
+def plot_layout(plot_title, plot_style, xlabel):
+  plt.style.use(plot_style) #e.g. ggplot, seaborn-colorblind, print(plt.style.available)
+  plt.title(plot_title)
+  plt.xlabel(r'${0}$'.format(xlabel))
+  plt.gca().spines['right'].set_visible(False)
+  plt.gca().spines['top'].set_visible(False)
+  plt.gca().yaxis.set_ticks_position('left')
+  plt.gca().xaxis.set_ticks_position('bottom')
+  plt.tight_layout()
+  
+def plot_histogram(df, col, xlabel, xmin, xmax, nbins, plot_title, plot_style, color, vline, out_path):
+  plt.close()
+  plt.figure()
+  bins = np.linspace(xmin, xmax, nbins)
+  df = df.toPandas()
+  plt.hist(df[col], bins, alpha=1, color=color)
+  if vline:
+    plt.axvline(x=vline, linestyle='dashed', linewidth=2.0, color='black')
+  plot_layout(plot_title, plot_style, xlabel)
+  plt.savefig(out_path)
+  plt.show()
+  
+def calculate_pval_bonferroni_cutoff(df, cutoff=0.05):
+  bonferroni_p =  cutoff / df.count()
+  return bonferroni_p
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Data Generation Constants
+
+# COMMAND ----------
+
+#genotype matrix
+n_samples = 500000
+n_variants = 1000
+
+#allele frequency
+allele_freq_cutoff = 0.05
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Data Storage Path Constants
+
+# COMMAND ----------
+
+user=dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
+dbfs_home_path_str = "dbfs:/home/{}/".format(user)
+dbfs_fuse_home_path_str = "/dbfs/home/{}/".format(user)
+dbfs_home_path = Path("dbfs:/home/{}/".format(user))
+dbfs_fuse_home_path = Path("/dbfs/home/{}/".format(user))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### set paths
+
+# COMMAND ----------
+
+output_delta = str(dbfs_home_path / f"genomics/data/delta/simulate_{n_samples}_samples_{n_variants}_variants_pvcf.delta")
+output_delta_transformed = str(dbfs_home_path / f"genomics/data/delta/simulate_{n_samples}_samples_{n_variants}_variants_pvcf_transformed.delta")
+output_hwe_plot = str(dbfs_fuse_home_path / f"genomics/data/results/simulate_{n_samples}_samples_{n_variants}_hwe.png")
+output_delta, output_delta_transformed, output_hwe_plot
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### prepare simulated delta table for GWAS
+
+# COMMAND ----------
+
+delta_vcf = spark.read.format("delta").load(output_delta)
+delta_gwas_vcf = (glow.transform('split_multiallelics', delta_vcf). \
+                  withColumn('values', glow.mean_substitute(glow.genotype_states('genotypes'))). \
+                  filter(fx.size(fx.array_distinct('values')) > 1)
+                 )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### perform quality control using glow helper functions
+
+# COMMAND ----------
+
+summary_stats_df = delta_gwas_vcf.select(
+    fx.expr("*"),
+    glow.expand_struct(glow.call_summary_stats(fx.col("genotypes"))),
+    glow.expand_struct(glow.hardy_weinberg(fx.col("genotypes")))
+  ). \
+    withColumn("log10pValueHwe", fx.when(fx.col("pValueHwe") == 0, 26).otherwise(-fx.log10(fx.col("pValueHwe"))))
+
+# COMMAND ----------
+
+display(summary_stats_df.drop("genotypes", "values"))
+
+# COMMAND ----------
+
+hwe_cutoff = calculate_pval_bonferroni_cutoff(summary_stats_df)
+display(plot_histogram(df=summary_stats_df.select("log10pValueHwe"), 
+                       col="log10pValueHwe",
+                       xlabel='-log_{10}(P)',
+                       xmin=0, 
+                       xmax=25, 
+                       nbins=50, 
+                       plot_title="hardy-weinberg equilibrium", 
+                       plot_style="ggplot",
+                       color='#e41a1c',
+                       vline = -np.log10(hwe_cutoff),
+                       out_path = output_hwe_plot
+                      )
+       )
+
+# COMMAND ----------
+
+variant_filter_df = summary_stats_df.where((fx.col("alleleFrequencies").getItem(0) >= allele_freq_cutoff) & 
+                                           (fx.col("alleleFrequencies").getItem(0) <= (1.0 - allele_freq_cutoff)) &
+                                           (fx.col("pValueHwe") >= hwe_cutoff)
+                                          )
+
+# COMMAND ----------
+
+variant_filter_df.write.mode("overwrite").format("delta").save(output_delta_transformed)
+
+# COMMAND ----------
+
+spark.read.format('delta').load(output_delta_transformed).count()
---
+++ docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE/tertiary/2_quickstart_quantitative_glowgr.py
@@ -0,0 +1,196 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC <img src="https://databricks.com/wp-content/themes/databricks/assets/images/databricks-logo.png" alt="logo" width="240"/> + <img src="https://www.regeneron.com/Content/images/science/regenron.png" alt="logo" width="240"/>
+# MAGIC 
+# MAGIC ### <img src="https://databricks-knowledge-repo-images.s3.us-east-2.amazonaws.com/HLS/glow/project_glow_logo.png" alt="logo" width="35"/> GloWGR: whole genome regression
+# MAGIC 
+# MAGIC ### Quantitative phenotypes
+# MAGIC 
+# MAGIC Recommended cluster setup: 
+# MAGIC large memory optimized virtual machines
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### library imports
+
+# COMMAND ----------
+
+import glow
+spark = glow.register(spark)
+import pyspark.sql.functions as fx
+
+import json
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Data Generation Constants
+
+# COMMAND ----------
+
+#genotype matrix
+n_samples = 500000
+n_variants = 1000
+
+#phenotypes
+n_quantitative_phenotypes = 1
+
+#covariates
+n_covariates = 10
+
+#wgr
+variants_per_block = 1000
+sample_block_count = 10
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Data Storage Path Constants
+
+# COMMAND ----------
+
+user=dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
+dbfs_home_path_str = "dbfs:/home/{}/".format(user)
+dbfs_fuse_home_path_str = "/dbfs/home/{}/".format(user)
+dbfs_home_path = Path("dbfs:/home/{}/".format(user))
+dbfs_fuse_home_path = Path("/dbfs/home/{}/".format(user))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC #### Prepare input paths.
+# MAGIC Glow can read variant data from common file formats like VCF, BGEN, and Plink.
+# MAGIC 
+# MAGIC Please checkpoint your data into Parquet or Delta Lake table before running GloWGR.
+# MAGIC 
+# MAGIC Note: The data in this problem is fabricated for demonstration purpose and is biologically meaningless.
+
+# COMMAND ----------
+
+delta_path = str(dbfs_home_path / 'genomics/data/delta/simulate_'.format(user))
+base_variants_path = output_delta = delta_path + str(n_samples) + '_samples_' + str(n_variants) + '_variants_pvcf.delta'
+variants_path = output_delta = delta_path + str(n_samples) + '_samples_' + str(n_variants) + '_variants_pvcf_transformed.delta'
+
+pandas_path = str(dbfs_fuse_home_path / ('genomics/data/pandas/simulate_'.format(user) + str(n_samples) + '_samples_'))
+covariates_path = pandas_path + str(n_covariates) + '_covariates.csv'
+quantitative_phenotypes_path = pandas_path + str(n_quantitative_phenotypes) + '_quantitative_phenotypes.csv'
+output_quantitative_offset = pandas_path + str(n_quantitative_phenotypes) + '_offset_quantitative_phenotypes.csv'
+
+sample_blocks_path = pandas_path + '_wgr_sample_blocks.json'
+block_matrix_path = delta_path + 'wgr_block_matrix.delta'
+y_hat_path = pandas_path + str(n_quantitative_phenotypes) + '_wgr_y_quantitative_hat.csv'
+
+# COMMAND ----------
+
+base_variant_df = spark.read.format('delta').load(base_variants_path)
+variant_df = spark.read.format('delta').load(variants_path)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC Extract sample IDs from a variant DataFrame with `glow.wgr.get_sample_ids`.
+
+# COMMAND ----------
+
+sample_ids = glow.wgr.get_sample_ids(base_variant_df)
+len(sample_ids)
+
+# COMMAND ----------
+
+# MAGIC 
+# MAGIC %md
+# MAGIC 
+# MAGIC Create the beginning block genotype matrix and sample block ID mapping with `glow.wgr.block_variants_and_samples`.
+# MAGIC 
+# MAGIC Write the block matrix to Delta and the sample blocks a JSON file so that we can reuse them for multiple phenotype batches.
+
+# COMMAND ----------
+
+block_df, sample_blocks = glow.wgr.block_variants_and_samples(variant_df, 
+                                                     sample_ids, 
+                                                     variants_per_block, 
+                                                     sample_block_count)
+
+# COMMAND ----------
+
+with open(sample_blocks_path, 'w') as f:
+  json.dump(sample_blocks, f)
+block_df.write.format('delta').mode('overwrite').save(block_matrix_path)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC #### Step 2: Run whole genome regression (WGR) to calculate expected phenotypes
+
+# COMMAND ----------
+
+block_df = spark.read.format('delta').load(block_matrix_path)
+with open(sample_blocks_path, 'r') as f:
+  sample_blocks = json.load(f)
+
+# COMMAND ----------
+
+display(block_df.limit(2))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC Read simulated phenotypes and covariates data from cloud storage (S3 or ADLS) as though it were on the local filesystem via the Databricks file system (DBFS). The phenotypes and covariates should have no missing values.
+
+# COMMAND ----------
+
+label_df = pd.read_csv(quantitative_phenotypes_path, 
+                       dtype={'sample_id': str}, 
+                       index_col='sample_id')[['QP1']]
+label_df.index = label_df.index.map(str) #the integer representation of sample_id was causing issues
+
+# COMMAND ----------
+
+label_df.head(5)
+
+# COMMAND ----------
+
+covariate_df = pd.read_csv(covariates_path, 
+                           dtype={'sample_id': str}, 
+                           index_col='sample_id')
+covariate_df.index = covariate_df.index.map(str) #ensures sample_ids are not represented as integers
+covariate_df.head(5)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC GloWGR runs best on small batches of phenotypes. Break the phenotype DataFrame into column chunks and generate
+# MAGIC leave-one-chromosome-out (LOCO) phenotype predictions for each chunk.
+
+# COMMAND ----------
+
+def chunk_columns(df, chunk_size):
+  for start in range(0, df.shape[1], chunk_size):
+    chunk = df.iloc[:, range(start, min(start + chunk_size, df.shape[1]))]
+    yield chunk
+
+# COMMAND ----------
+
+chunk_size = 10
+loco_estimates = []
+for label_df_chunk in chunk_columns(label_df, chunk_size):
+  loco_estimates.append(glow.wgr.estimate_loco_offsets(block_df, label_df_chunk, sample_blocks, covariate_df))
+
+# COMMAND ----------
+
+all_traits_loco_df = pd.concat(loco_estimates, axis='columns')
+all_traits_loco_df.to_csv(y_hat_path)
+
+# COMMAND ----------
+
+test = pd.read_csv(y_hat_path)
+test
