805cf7f8555d75ff9f8358a2e84e13abc6372293
William Brandler <William.Brandler@databricks.com>
2021-11-18 16:49:14 -0800

    streamline benchmarks notebook
    Signed-off-by: William Brandler <William.Brandler@databricks.com>

--- docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE/tertiary/8_pipeline_runs_comparison.py
+++ docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE/tertiary/8_pipeline_runs_comparison.py
@@ -64,23 +64,6 @@ display(benchmark_results.where((fx.col("test") == ("linear_regression")) |
 
 # MAGIC %md
 # MAGIC ##### Glow pipeline steps breakdown
-# MAGIC 
-# MAGIC The majority of compute is spent on `ingest`, `etl`, and `quality control`, but these steps are only run one or a few times at scale.
-# MAGIC 
-# MAGIC `Regressions` (or other analytics, such as emprically deriving P values from simulations) are typically run many times.
-# MAGIC 
-# MAGIC TODO: flame graph of the executors
-# MAGIC 
-# MAGIC where are the executors spending their time
-# MAGIC 
-# MAGIC garbage collection when dealing with arrays, convert ints to shorts
-# MAGIC 
-# MAGIC transforming large arrays into smaller versions
-# MAGIC 
-# MAGIC sample filtering biggest bottleneck.
-# MAGIC For regressions can pass in sample Ids, under the hood does the masking of the samples
-# MAGIC For offset generation it is more complicated. But this is on a smaller dataset, ~500k variants.
-# MAGIC But you have to do it per phenotype. So it adds up if you do a lot of phenotypes.
 
 # COMMAND ----------
 
