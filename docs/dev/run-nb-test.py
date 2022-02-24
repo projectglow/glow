ff34bce8883707210ee3b4bcccf0a79e1fb1e5a9
William Brandler <william.brandler@databricks.com>
2022-02-23 17:31:35 -0800

    move to multitask jobs for notebook continuous integrations
    Signed-off-by: William Brandler <william.brandler@databricks.com>

--- docs/dev/run-nb-test.py
+++ docs/dev/run-nb-test.py
@@ -1,10 +1,14 @@
 '''
-Runs all Glow notebooks in Databricks as an integration test.
+Runs all Glow notebooks as an integration test in Databricks using 
+1. Databricks CLI to set up continuous integration
+  - Repos to sync notebooks from the Glow repository to your workspace https://docs.databricks.com/dev-tools/cli/repos-cli.html#repos-cli
+  - multitask jobs to define the pipeline and run using Databricks Jobs API 2.1
 
-Before running this, configure your Databricks CLI profile.
+Before running, configure your Databricks CLI profile https://docs.databricks.com/dev-tools/cli/index.html#set-up-authentication 
+Examples of this can be found in the circleci configuration in the top level of the glow repo at `.circleci/config.yml`
 
 Example usage:
-  python3 docs/dev/run-nb-test.py --cli-profile docs-ci --nbs etl/merge-vcf --nbs tertiary/pandas-lmm
+  python3 docs/dev/run-nb-test.py --cli-profile <databricks cli profile> --json <configuration json>
 '''
 import click
 from datetime import datetime
@@ -16,8 +20,7 @@ import sys
 import time
 import uuid
 
-JOBS_JSON = 'docs/dev/jobs-config.json'
-NOTEBOOK_JOBS_JSON_MAPPING = 'docs/dev/notebook-jobs-config-mapping.json'
+JOBS_JSON = 'docs/dev/multitask-integration-test-config.json'
 
 def run_cli_cmd(cli_profile, api, args):
     cmd = ['databricks', '--profile', cli_profile, api] + args
@@ -26,25 +29,16 @@ def run_cli_cmd(cli_profile, api, args):
         raise ValueError(res)
     return res.stdout
 
-def get_jobs_config(d, key, jobs_path="docs/dev/jobs-config.json"):
-    """
-    :param d: dictionary with mapping of notebooks to databricks jobs configuration (from NOTEBOOK_JOBS_JSON_MAPPING)
-    :param key: notebook (nb) name
-    :jobs_path: path to default jobs configuration to test notebooks
-    """
-    if key in d:
-         jobs_path = d[key] 
-    print("running notebook " + key + " with the following jobs configuration json " + jobs_path)
-    return jobs_path
 
 @click.command()
 @click.option('--cli-profile', default='DEFAULT', help='Databricks CLI profile name.')
+@click.option('--repos-path', default='/Repos/staging/', help='Path in Databricks workspace for running integration test')
 @click.option('--workspace-tmp-dir', default='/tmp/glow-nb-test-ci', help='Base workspace dir for import and testing.')
 @click.option('--source-dir', default='docs/source/_static/zzz_GENERATED_NOTEBOOK_SOURCE',
               help='Source directory of notebooks to upload.')
 @click.option('--nbs', multiple=True, default=[],
               help='Relative name of notebooks in the source directory to run. If not provided, runs all notebooks.')
-def main(cli_profile, workspace_tmp_dir, source_dir, nbs):
+def main(cli_profile,repos_path, workspace_tmp_dir, source_dir, nbs):
     identifier = str(uuid.uuid4())
     work_dir = os.path.join(workspace_tmp_dir, identifier)
     with open(JOBS_JSON, 'r') as f:
@@ -60,54 +54,49 @@ def main(cli_profile, workspace_tmp_dir, source_dir, nbs):
     nb_to_run_info = {}
 
     try:
-        print(f"Importing source files from {source_dir} to {work_dir}")
-        run_cli_cmd(cli_profile, 'workspace', ['mkdirs', work_dir])
-        run_cli_cmd(cli_profile, 'workspace', ['import_dir', source_dir, work_dir])
+        print(f"Importing source files from Glow repo")
+        run_cli_cmd(cli_profile, 'workspace', ['mkdirs', repos_path])
+        run_cli_cmd(cli_profile, 'repos', ['create', '--url', 'https://github.com/projectglow/glow', '--provider', 'gitHub', '--path', repos_path + "glow"])
 
         print(f"Launching runs")
-        for nb in nbs:
-            jobs_json_path = get_jobs_config(notebook_jobs_json_mapping, nb)
-            with open(jobs_json_path, 'r') as f:
-                jobs_json = json.load(f)
-            jobs_json['name'] = 'Glow notebook integration test - ' + nb
-            jobs_json['notebook_task'] = {'notebook_path': work_dir + '/' + nb}
-            run_submit = run_cli_cmd(cli_profile, 'runs', ['submit', '--json', json.dumps(jobs_json)])
-            run_id = json.loads(run_submit)['run_id']
-            nb_to_run_id[nb] = str(run_id)
-            while True:
-                print(f"=== Status check at {datetime.now().strftime('%H:%M:%S')} ===")
-                for nb, run_id in nb_to_run_id.items():
-                    run_get = run_cli_cmd(cli_profile, 'runs', ['get', '--run-id', run_id])
-                    run_info = json.loads(run_get)
-                    nb_to_run_info[nb] = run_info
-                for nb, run_info in nb_to_run_info.items():
-                    base_msg = f"{nb} (Run ID {nb_to_run_id[nb]}) [{run_info['state']['life_cycle_state']}]"
-                    if run_info['state']['life_cycle_state'] == 'INTERNAL_ERROR':
-                       print(base_msg, run_info['state']['result_state'], run_info['run_page_url'])
-                       print("====================================")
-                       print("|  Exiting due to internal error.  |")
-                       print("====================================")
-                       sys.exit(1)
-                    if run_info['state']['life_cycle_state'] == 'TERMINATED':
-                        print(base_msg, run_info['state']['result_state'], run_info['run_page_url'])
-                        if run_info['state']['result_state'] == 'FAILED':
-                            print("===========================")
-                            print("|    Some tasks failed.    |")
-                            print("============================")
-                            sys.exit(1)
-                        else:
-                            print(base_msg, run_info['state']['result_state'])
-                    else:
-                        print(base_msg, run_info['state']['state_message'])
-                if all([run_info['state']['life_cycle_state'] == 'TERMINATED' for run_info in nb_to_run_info.values()]):
-                    break
-                time.sleep(60)
+        jobs_json_path = get_jobs_config(notebook_jobs_json_mapping, nb)
+        with open(jobs_json_path, 'r') as f:
+            jobs_json = json.load(f)
+        job_create = run_cli_cmd(cli_profile, 'jobs', ['create', '--json-file', json.dumps(jobs_json)])
+        job_id = json.loads(job_create)['job_id']
+        job_run = run_cli_cmd(cli_profile, 'jobs', ['run-now', '--job-id', job_id)
+        run_id = json.loads(job_run)['run_id']
+        while True:
+            print(f"=== Status check at {datetime.now().strftime('%H:%M:%S')} ===")
+            run_get = run_cli_cmd(cli_profile, 'runs', ['get', '--run-id', run_id])
+            run_info = json.loads(run_get)
+            base_msg = f"{nb} (Run ID {nb_to_run_id[nb]}) [{run_info['state']['life_cycle_state']}]"
+            if run_info['state']['life_cycle_state'] == 'INTERNAL_ERROR':
+               print(base_msg, run_info['state']['result_state'], run_info['run_page_url'])
+               print("====================================")
+               print("|  Exiting due to internal error.  |")
+               print("====================================")
+               sys.exit(1)
+            if run_info['state']['life_cycle_state'] == 'TERMINATED':
+                print(base_msg, run_info['state']['result_state'], run_info['run_page_url'])
+                if run_info['state']['result_state'] == 'FAILED':
+                    print("===========================")
+                    print("|    Some tasks failed.    |")
+                    print("============================")
+                    sys.exit(1)
+                else:
+                    print(base_msg, run_info['state']['result_state'])
+            else:
+                print(base_msg, run_info['state']['state_message'])
+            if all([run_info['state']['life_cycle_state'] == 'TERMINATED' for run_info in nb_to_run_info.values()]):
+                break
+            time.sleep(60)
     finally:
         if all([run_info['state']['result_state'] == 'SUCCESS' for run_info in nb_to_run_info.values()]):
             print("===========================")
             print("|   All tasks succeeded!   |")
             print("============================")
-            run_cli_cmd(cli_profile, 'workspace', ['rm', '-r', work_dir])
+            run_cli_cmd(cli_profile, 'repos', ['delete', '--path', repos_path + "glow"])
             sys.exit(0)
 
 if __name__ == '__main__':
