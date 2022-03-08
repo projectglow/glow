c1175e9f29ac0f474cef8b67b4946c89f5a64fe7
William Brandler <william.brandler@databricks.com>
2022-03-08 11:51:35 -0800

    copy 3.1 shim into 3.2
    Signed-off-by: William Brandler <william.brandler@databricks.com>

--- core/src/main/shim/3.2/SparkShim.scala
+++ core/src/main/shim/3.2/SparkShim.scala
@@ -20,6 +20,41 @@ import org.apache.spark.sql.catalyst.expressions._
 
 // Spark 3.2 APIs that are not inter-version compatible
 object SparkShim extends SparkShimBase {
+  // [SPARK-25393][SQL] Adding new function from_csv()
+  // Refactors classes from [[org.apache.spark.sql.execution.datasources.csv]] to [[org.apache.spark.sql.catalyst.csv]]
+  override type CSVOptions = org.apache.spark.sql.catalyst.csv.CSVOptions
+  override type UnivocityParser = org.apache.spark.sql.catalyst.csv.UnivocityParser
+
+  override def wrapUnivocityParse(parser: UnivocityParser)(input: String): Option[InternalRow] = {
+    parser.parse(input)
+  }
+
+  // [SPARK-27328][SQL] Add 'deprecated' in ExpressionDescription for extended usage and SQL doc
+  // Adds 'deprecated' argument to the ExpressionInfo constructor
+  override def createExpressionInfo(
+      className: String,
+      db: String,
+      name: String,
+      usage: String,
+      arguments: String,
+      examples: String,
+      note: String,
+      since: String): ExpressionInfo = {
+    // TODO fix this up later.
+    new ExpressionInfo(
+      className,
+      db,
+      name,
+      usage,
+      arguments
+    )
+  }
+
+  // [SPARK-28077][SQL] Support ANSI SQL OVERLAY function.
+  // Adds QuaternaryExpression
+  abstract class QuaternaryExpression
+      extends org.apache.spark.sql.catalyst.expressions.QuaternaryExpression
+
 
   override def children: Seq[Expression] = arguments ++ functions
 
