ddccc3088dafc709a3ca07d003158546e15e3e49
William Brandler <william.brandler@databricks.com>
2022-03-22 15:27:07 -0700

    break up into layers and add genomics libs
    Signed-off-by: William Brandler <william.brandler@databricks.com>

--- docker/open-source-glow/build.sh
+++ docker/open-source-glow/build.sh
@@ -6,5 +6,7 @@
 DOCKER_REPOSITORY="projectglow"
 
 # Add commands to build DBR 9.1 images below
-docker build -t "${DOCKER_REPOSITORY}/open-source-glow:1.1.2" datamechanics/
+docker build -t "${DOCKER_REPOSITORY}/open-source-base:1.1.2" datamechanics/
+docker build -t "${DOCKER_REPOSITORY}/open-source-genomics:1.1.2" genomics/
+docker build -t "${DOCKER_REPOSITORY}/open-source-glow:1.1.2" genomics-with-glow/
 docker push "${DOCKER_REPOSITORY}/open-source-glow:1.1.2"
